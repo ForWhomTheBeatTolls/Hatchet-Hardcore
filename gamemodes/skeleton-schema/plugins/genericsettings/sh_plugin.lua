6f5e09fe6e8465fc34525dea96f9bb515046e70c
willmaster <79755822+willmasterLUA@users.noreply.github.com>
2024-05-29 22:35:12 +0200

    hud_ambience

--- gamemodes/skeleton-schema/plugins/genericsettings/sh_plugin.lua
+++ gamemodes/skeleton-schema/plugins/genericsettings/sh_plugin.lua
@@ -1,3 +1,4 @@
 impulse.DefineSetting("hud_jim", {name="Movement Indicator", category="HUD", type="tickbox", default=false})
 impulse.DefineSetting("hud_hunger", {name="Hunger Bar", category="HUD", type="tickbox", default=true})
+impulse.DefineSetting("hud_ambience", {name="Ambience Color", category="HUD", type="tickbox", default=true})
 impulse.DefineSetting("admin_dispatch", {name="Dispatch", category="ops", type="tickbox", default=false})
