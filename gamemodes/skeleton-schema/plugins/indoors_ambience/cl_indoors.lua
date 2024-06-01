6430dac8f4fabfc9c8cadf6565a94845f0e9b05a
willmaster <79755822+willmasterLUA@users.noreply.github.com>
2024-06-01 15:43:25 +0200

    Forgot to remove this

--- gamemodes/skeleton-schema/plugins/indoors_ambience/cl_indoors.lua
+++ gamemodes/skeleton-schema/plugins/indoors_ambience/cl_indoors.lua
@@ -12,7 +12,7 @@ hook.Add( "PostDrawTranslucentRenderables", "trace_visualize", function()
 	--render.DrawLine( eyePos, outdoorscheck.HitPos, color_red, true )
 
 	-- Show that the traceline is a line, not a hull
-	render.DrawWireframeBox( outdoorscheck.HitPos, Angle( 0, 0, 0 ), mins, maxs, color_red, true )
+	--render.DrawWireframeBox( outdoorscheck.HitPos, Angle( 0, 0, 0 ), mins, maxs, color_red, true )
 	-- listen here GLUA, if you dont fucking play how i want it to i will rape your wife and make you suffer by adding 500 hook.add(thinks)
 	-- do you understand what im saying you faggot?
 
