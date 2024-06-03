852dcf64885b5ae4d7977ad5c78d9e4edbe321e0
ForWhomTheBeatTolls <155327198+ForWhomTheBeatTolls@users.noreply.github.com>
2024-06-03 16:09:02 +0300

    The worst code mankind will ever see

--- gamemodes/skeleton-schema/entities/entities/m_npcspawner.lua
+++ gamemodes/skeleton-schema/entities/entities/m_npcspawner.lua
@@ -11,89 +11,82 @@ ENT.AdminOnly = true
 ENT.Author = "willmasterr & SteveB"
 
 function ENT:Initialize()
-	-- Sets what model to use
+
 	if SERVER then
-	for k,v in ipairs (ents.FindByClass( "m_npcspawner")) do
-			self:SetName("m_npcspawner"..k)
-	end
-
-		print("The entitys name is.. "..self:GetName())
 	
-	self:SetModel( "models/props_junk/sawblade001a.mdl" )
+		for k,v in ipairs (ents.FindByClass( "m_npcspawner")) do
+			self:SetName("m_npcspawner"..k)
+		end
+		
+		--print("The entitys name is.. "..self:GetName())
+		self:SetModel( "models/props_junk/sawblade001a.mdl" )
 	end
-
-
-
-	-- Sets what color to use
 
 	self:SetRenderMode( RENDERMODE_TRANSCOLOR )
 	self:SetColor(Color(0, 0, 0))
 
-	
-	--self:SetMaterial("models/player/shared/gold_player")
-
-
-	-- Physics stuff
-	--self:SetMoveType( MOVETYPE_VPHYSICS )
-	--self:SetSolid( SOLID_VPHYSICS )
-
-	-- Init physics only on server, so it doesn't mess up physgun beam
 	if ( SERVER ) then self:PhysicsInit( SOLID_VPHYSICS ) end
 	
-	-- Make prop to fall on spawn
 	phys = self:GetPhysicsObject()
-	if ( IsValid( phys ) ) then 
-        --phys:Wake()
-    end
 	
-end
-
-local delay = 0
-local amount = 0
-function ENT:Think()
-	if CurTime() < delay then return end
-
-	npclist = {
+	local npclist = {
 		"npc_fastzombie",
 		"npc_zombie"
 	}
-
-	function ShowSpawnerPosition()	
-
-		if CLIENT then
-			if LocalPlayer():GetMoveType() == MOVETYPE_NOCLIP and LocalPlayer():IsAdmin() then
-				self:SetColor(Color(0, 255, 0, 255))
-
-			else
-				self:SetColor(Color(0, 255, 0, 0))
-			end
-		end	
-
-	end
 	
-	function NPCRespawnCheck()	
-	if SERVER then
-		amount = amount + 1
-		if ( IsValid(npcspawned) ) then
-			return
+end
+	
+local delay = 0
+local amount = 0
+local npcexists = false
+function ENT:Think()
+
+if CurTime() < delay then return end
+
+if CLIENT then
+for k,v in pairs(ents.FindByClass("m_npcspawner*")) do
+	if LocalPlayer():GetMoveType() == MOVETYPE_NOCLIP and LocalPlayer():IsAdmin() then
+		v:SetColor(Color(0, 255, 0, 255))
+	else
+		v:SetColor(Color(0, 255, 0, 0))
+	end
+end
+end
+	
+if SERVER then
+	for _, n in ipairs(ents.FindByName(self:GetName().."_*")) do
+		if not n then
+			--print("NOT N: "..n)
+			npcexists = false
 		else
-			--print("The NPC Is Dead")
+			--print("YES N: "..n)
+			npcexists = true
+		end
+	end
+		if npcexists == false then
 			local npcspawned = ents.Create( npclist[ math.random( #npclist ) ] )
 			npcspawned:SetPos(self:GetPos())
 			npcspawned:Spawn()
-			npcspawned:SetName("m_npc_spawned_"..npcspawned:GetName().."_"..amount)
+			npcspawned:SetName(self:GetName().."_"..npcspawned:GetClass())
+			--print(npcspawned:GetName())
+		else
+			npcexists = false
 		end
-	end
-	end
-	
-	NPCRespawnCheck()
-	ShowSpawnerPosition()
+end
+		
+	--self:ShowSpawnerPosition()
 	
 	delay = CurTime() + 4
+		
 end
 
 function ENT:OnRemove()
-	for _, npc in pairs(ents.FindByName("m_npc_spawned_*")) do
-		npc:Remove()
+	if SERVER then
+		--print(self:GetName())
+		--print(npcspawned:GetName())
+		for _, npc in pairs(ents.FindByName(self:GetName().."_*")) do
+			npc:Remove()
+		end
 	end
 end
+
