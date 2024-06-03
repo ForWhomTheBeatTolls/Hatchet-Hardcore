022dfa83c276bb2ec36e57c67b567a1d81c5346b
ForWhomTheBeatTolls <155327198+ForWhomTheBeatTolls@users.noreply.github.com>
2024-06-03 13:41:53 +0300

    Infinitely better spawner

--- gamemodes/skeleton-schema/entities/entities/m_npcspawner.lua
+++ gamemodes/skeleton-schema/entities/entities/m_npcspawner.lua
@@ -8,34 +8,25 @@ ENT.PrintName = "NPC Spawner"
 ENT.Category = "impulse"
 ENT.Spawnable = true
 ENT.AdminOnly = true
-ENT.Author = "willmasterr"
+ENT.Author = "willmasterr & SteveB"
 
 function ENT:Initialize()
 	-- Sets what model to use
+	if SERVER then
+	for k,v in ipairs (ents.FindByClass( "m_npcspawner")) do
+			self:SetName("m_npcspawner"..k)
+	end
+
+		print("The entitys name is.. "..self:GetName())
+	
 	self:SetModel( "models/props_junk/sawblade001a.mdl" )
+	end
 
 
 
 	-- Sets what color to use
-	local delay = 0
 
 	self:SetRenderMode( RENDERMODE_TRANSCOLOR )
-
-	hook.Add("Think", "ShowSpawnerPosition", function()
-		if CurTime() < delay then return end	
-
-		if CLIENT then
-			if LocalPlayer():GetMoveType() == MOVETYPE_NOCLIP and LocalPlayer():IsAdmin() then
-				self:SetColor(Color(0, 255, 0, 255))
-
-			else
-				self:SetColor(Color(0, 255, 0, 0))
-			end
-		end	
-
-		delay = CurTime() + 4 -- Makes every 4 secs run the code
-	end)
-
 	self:SetColor(Color(0, 0, 0))
 
 	
@@ -54,39 +45,55 @@ function ENT:Initialize()
 	if ( IsValid( phys ) ) then 
         --phys:Wake()
     end
+	
+end
 
-	local npclist = {
+local delay = 0
+local amount = 0
+function ENT:Think()
+	if CurTime() < delay then return end
+
+	npclist = {
 		"npc_fastzombie",
 		"npc_zombie"
 	}
-	if SERVER then
 
-		npcspawned = ents.Create( npclist[ math.random( #npclist ) ] )
-		npcspawned:SetPos(self:GetPos())
-		npcspawned:Spawn()
-	
-		for k,v in ipairs (ents.FindByClass( "m_npcspawner")) do
-			self:SetName("m_npcspawner"..k)
-		end
+	function ShowSpawnerPosition()	
 
-		print("The entitys name is.. "..self:GetName())
-	
-		hook.Add("Think", "NPCRespawnCheck", function()
-			if CurTime() < delay then return end	
-	
-			if ( IsValid(npcspawned) ) then
-				--print("The NPC Is alive")
+		if CLIENT then
+			if LocalPlayer():GetMoveType() == MOVETYPE_NOCLIP and LocalPlayer():IsAdmin() then
+				self:SetColor(Color(0, 255, 0, 255))
+
 			else
-				--print("The NPC Is Dead")
-				npcspawned = ents.Create( npclist[ math.random( #npclist ) ] )
-				npcspawned:SetPos(self:GetPos())
-				npcspawned:Spawn()
+				self:SetColor(Color(0, 255, 0, 0))
 			end
-	
-			delay = CurTime() + 0.1 -- Makes every 4 secs run the code
-		end)
+		end	
+
 	end
+	
+	function NPCRespawnCheck()	
+	if SERVER then
+		amount = amount + 1
+		if ( IsValid(npcspawned) ) then
+			return
+		else
+			--print("The NPC Is Dead")
+			local npcspawned = ents.Create( npclist[ math.random( #npclist ) ] )
+			npcspawned:SetPos(self:GetPos())
+			npcspawned:Spawn()
+			npcspawned:SetName("m_npc_spawned_"..npcspawned:GetName().."_"..amount)
+		end
+	end
+	end
+	
+	NPCRespawnCheck()
+	ShowSpawnerPosition()
+	
+	delay = CurTime() + 4
+end
 
-
-	self:CallOnRemove("RemoveNPC", function() hook.Remove("Think", "NPCRespawnCheck") hook.Remove("Think", "ShowSpawnerPosition") end)
+function ENT:OnRemove()
+	for _, npc in pairs(ents.FindByName("m_npc_spawned_*")) do
+		npc:Remove()
+	end
 end
