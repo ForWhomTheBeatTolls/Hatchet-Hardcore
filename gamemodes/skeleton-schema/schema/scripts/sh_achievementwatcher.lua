2b7cff005f052f56b7caff54c142a966887ab225
ForWhomTheBeatTolls <155327198+ForWhomTheBeatTolls@users.noreply.github.com>
2024-06-04 21:57:30 +0300

    Create sh_achievementwatcher.lua

---
+++ gamemodes/skeleton-schema/schema/scripts/sh_achievementwatcher.lua
@@ -0,0 +1,27 @@
+if SERVER then
+	hook.Add("PlayerDeath", "impulseAchDeath", function(ply, inflictor, attacker)
+	if SERVER then
+		if not ply:Alive() then
+			ply:AchievementGive("ach_adie")
+		end
+		if not ply == attacker then
+			attacker:AchievementGive("ach_akill")
+		end
+		if ply:Team() == TEAM_OTA and (not ply == attacker) then
+			attacker:AchievementGive("ach_combine2")
+		end
+		if ply:Team() == TEAM_CP and (not ply == attacker) then
+			attacker:AchievementGive("ach_combine1")
+		end
+	end
+end)
+	hook.Add("PlayerSay", "impulseChatAchWatch", function(ply, text)
+			if
+		text == "I keep falling, but never falling six feet deep." or
+		text == "I keep falling, but never falling six feet deep" or
+		text == "i keep falling, but never falling six feet deep." or
+		text == "i keep falling, but never falling six feet deep" then
+		ply:AchievementGive("ach_6feet")
+			end
+		end)
+    end
