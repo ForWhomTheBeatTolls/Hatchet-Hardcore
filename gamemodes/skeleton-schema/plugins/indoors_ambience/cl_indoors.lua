31e9e6473410fe6c8c28c2d2fe5a5038fc96bbbb
ForWhomTheBeatTolls <155327198+ForWhomTheBeatTolls@users.noreply.github.com>
2024-06-04 21:53:45 +0300

    Improved ambience & readded ambience color

--- gamemodes/skeleton-schema/plugins/indoors_ambience/cl_indoors.lua
+++ gamemodes/skeleton-schema/plugins/indoors_ambience/cl_indoors.lua
@@ -1,38 +1,55 @@
-local color_red = Color( 255, 0, 0 )
-local mins, maxs = Vector( -4, -4, -4 ), Vector( 4, 4, 4 )
-
-hook.Add( "PostDrawTranslucentRenderables", "trace_visualize", function()
-	local eyePos = Entity( 1 ):GetPos()
-
+function IsIndoors()
+	local day = CreateSound(LocalPlayer(), "ambient/forest_day.wav")
+	local eyePos = LocalPlayer():GetPos()
 	local outdoorscheck = util.TraceLine( {
-		start = eyePos,
-		endpos = eyePos + Vector(0, 0, 9999),
-		filter = Entity( 1 )
-	} )
-	--render.DrawLine( eyePos, outdoorscheck.HitPos, color_red, true )
-
-	-- Show that the traceline is a line, not a hull
-	--render.DrawWireframeBox( outdoorscheck.HitPos, Angle( 0, 0, 0 ), mins, maxs, color_red, true )
-
-	function IsIndoors()
-		local night = CreateSound(LocalPlayer(), "ambient/forest_night.wav")
-		if outdoorscheck.HitSky == true then
-			--print("true")
-			night:PlayEx(0.17, 60)
-		else
-			--print("false")
-			night:Stop()
-		end
+	start = eyePos,
+	endpos = eyePos + Vector(0, 0, 99999),
+	filter = LocalPlayer()
+} )
+	if outdoorscheck.HitSky then
+		--print("true")
+		day:PlayEx(0.17, 60)
+		return false
+	else
+		--print("false")
+		day:Stop()
+		return true
 	end
+end
 
 
+local delay = CurTime()
+hook.Add("Think", "AmbienceInDoors", function()
+	
+	local ambience = {
+	["$pp_colour_addr"] = 0,
+	["$pp_colour_addg"] = 0,
+	["$pp_colour_addb"] = 0,
+	["$pp_colour_brightness"] = -0.04,
+	["$pp_colour_contrast"] = 1.4,
+	["$pp_colour_colour"] = 0.8,
+	["$pp_colour_mulr"] = 0,
+	["$pp_colour_mulg"] = 0,
+	["$pp_colour_mulb"] = 0
+	}
 
-end)
+	if CurTime() > delay then 
+		IsIndoors()
+		--print(tobool(IsIndoors()))
+		if impulse.GetSetting("hud_ambience") then
+			if IsIndoors() == false then
+				hook.Add("RenderScreenspaceEffects", "HatchetAmbienceColor", function()
+					DrawColorModify( ambience )
+				end )
+			else
+				hook.Remove("RenderScreenspaceEffects", "HatchetAmbienceColor")
+			end
+		end
 
-
-local delay = 0
-hook.Add("Think", "AmbienceOutside", function()
-	if CurTime() < delay then return end	
-    IsIndoors()
-	delay = CurTime() + 2 -- Makes every 4 secs run the code
+		if not impulse.GetSetting("hud_ambience") then
+			hook.Remove("RenderScreenspaceEffects", "HatchetAmbienceColor")
+		end
+		
+		delay = CurTime() + 1
+	end
 end)
