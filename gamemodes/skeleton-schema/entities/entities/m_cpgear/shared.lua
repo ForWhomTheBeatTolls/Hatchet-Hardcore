2625f501d6e7e005c10a6d4df18e925865d8c2bb
ForWhomTheBeatTolls <155327198+ForWhomTheBeatTolls@users.noreply.github.com>
2024-06-06 19:53:05 +0300

    Create shared.lua

---
+++ gamemodes/skeleton-schema/entities/entities/m_cpgear/shared.lua
@@ -0,0 +1,10 @@
+ENT.Type = "anim"
+ENT.PrintName = "Civil Protection Locker"
+ENT.Author = "mario & muhammed"
+ENT.Category = "Hatchet"
+ENT.Spawnable = true
+ENT.AdminOnly = true
+ENT.Base = "base_gmodentity"
+
+ENT.HUDName = "Civil Protection Locker"
+ENT.HUDDesc = ""
