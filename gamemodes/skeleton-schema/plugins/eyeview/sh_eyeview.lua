64c1aeeab12aad2c070bee96a5dfcb52a2466d62
willmaster <79755822+willmasterLUA@users.noreply.github.com>
2024-06-06 19:22:58 +0200

    Fix Eyeview

--- gamemodes/skeleton-schema/plugins/eyeview/sh_eyeview.lua
+++ gamemodes/skeleton-schema/plugins/eyeview/sh_eyeview.lua
@@ -91,20 +91,15 @@ function EYEVIEW_CalcView( ply, origin, angles, fov, near, far )
 		hent2 = hc2.Entity
 		hit2 = hc2.Hit
 		
-		local clipcheck = {}
-	clipcheck.start = LocalPlayer():GetBonePosition(LocalPlayer():LookupBone("ValveBiped.Bip01_Head1")) + Vector(1,0,-6)
-	clipcheck.endpos = LocalPlayer():GetBonePosition(LocalPlayer():LookupBone("ValveBiped.Bip01_Head1")) + Vector(1,0,-1)
-	clipcheck.filter = function(ent)
-		if ( ent:GetClass() == "player" ) then
-			return false
-		end
-	end
-	clipcheck.mins = Vector( -7.5, -7.5, -7.5 )
-	clipcheck.maxs = Vector( 7.5, 7.5, 7.5 )
-	clipcheck.mask = LocalPlayer()
-	
-		local cccheck = util.TraceHull(clipcheck)
-		local clipcheckhit = cccheck.Hit
+		local clipcheck = util.TraceHull( {
+	start = LocalPlayer():GetBonePosition(LocalPlayer():LookupBone("ValveBiped.Bip01_Head1")) + Vector(1,0,-3)	,
+	endpos = LocalPlayer():GetBonePosition(LocalPlayer():LookupBone("ValveBiped.Bip01_Head1")) + Vector(1,0,-3) ,
+	filter = LocalPlayer()	,
+	mins = Vector( -7.5, -7.5, -7.5 ),
+	maxs = Vector( 7.5, 7.5, 7.5 ),
+	mask = LocalPlayer()
+} )
+		local clipcheckhit = clipcheck.Hit
 		
 		---------------------###CHECKS END###---------------------
 		
@@ -229,6 +224,7 @@ end
 end
 hook.Add( "ShouldDrawLocalPlayer", "EYEVIEW_ShouldDrawLocalPlayer", EYEVIEW_ShouldDrawLocalPlayer )
 
+/*
 function EYEVIEW_HUDPaint()
 
     if LocalPlayer():IsValid() && LocalPlayer():Alive() then
@@ -249,4 +245,7 @@ function EYEVIEW_HUDPaint()
 end
 --hook.Add( "HUDPaint", "EYEVIEW_HUDPaint", EYEVIEW_HUDPaint )
 
+
+
+*/
 end
