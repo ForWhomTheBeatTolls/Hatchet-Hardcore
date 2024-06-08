1edb9b8cedbbedfbc9525018420a75eca95529bd
WillMaster <79755822+willmasterLUA@users.noreply.github.com>
2024-06-08 19:00:51 +0200

    Fix Indoors, sound not great though..

--- gamemodes/skeleton-schema/plugins/indoors_ambience/cl_indoors.lua
+++ gamemodes/skeleton-schema/plugins/indoors_ambience/cl_indoors.lua
@@ -1,68 +1,75 @@
 function IsIndoors()
-	
+
 	local eyePos = LocalPlayer():GetPos()
 	local outdoorscheck = util.TraceLine( {
 	start = eyePos,
 	endpos = eyePos + Vector(0, 0, 99999),
 	filter = LocalPlayer()
 } )
-	if outdoorscheck.HitSky then
-		--print("true")
-		return false
-	else
-		--print("false")
-		
-		return true
-	end
+
+	indoor = outdoorscheck.HitSky
 end
 
 
 
-
-local delay = CurTime()
-hook.Add("Think", "AmbienceInDoors", function()
+local doneItOnce = false
+function DaMachine()
 
 	local day = CreateSound(LocalPlayer(), "ambient/forest_day.wav")
 	local ambience = {
-		["$pp_colour_addr"] = 0,
-		["$pp_colour_addg"] = 0,
-		["$pp_colour_addb"] = 0,
-		["$pp_colour_brightness"] = -0.04,
-		["$pp_colour_contrast"] = 1.4,
-		["$pp_colour_colour"] = 0.8,
-		["$pp_colour_mulr"] = 0,
-		["$pp_colour_mulg"] = 0,
-		["$pp_colour_mulb"] = 0
-		}
+	["$pp_colour_addr"] = 0,
+	["$pp_colour_addg"] = 0,
+	["$pp_colour_addb"] = 0,
+	["$pp_colour_brightness"] = -0.04,
+	["$pp_colour_contrast"] = 1.4,
+	["$pp_colour_colour"] = 0.8,
+	["$pp_colour_mulr"] = 0,
+	["$pp_colour_mulg"] = 0,
+	["$pp_colour_mulb"] = 0
+	}
 
-	if CurTime() > delay then 
-		IsIndoors()
-		--print(tobool(IsIndoors()))
-		if impulse.GetSetting("hud_ambience") then
-			if IsIndoors() == false then
-				day:PlayEx(0.17, 60)
+	IsIndoors()
+
+	if indoor then
+		day:PlayEx(1, 60)
+		if not doneItOnce then
+			doneItOnce = true
+			if impulse.GetSetting("hud_ambience") then
 				hook.Add("RenderScreenspaceEffects", "HatchetAmbienceColor", function()
 					DrawColorModify( ambience )
 				end )
-				--print("Outside!")
-			else
-				--print("Inside!")
-				day:FadeOut(1)
-				timer.Simple(6,function() day:Stop() end)
-				hook.Remove("RenderScreenspaceEffects", "HatchetAmbienceColor")
 			end
+			//print("Outside")
 		end
+	end
+
+	if not indoor then
+		day:Stop()
+		if doneItOnce then
+			doneItOnce = false
+			hook.Remove("RenderScreenspaceEffects", "HatchetAmbienceColor")
+			//print("Inside")
+		end
+	end
+end
+
+
+local delay = CurTime()
+hook.Add("Think", "AmbienceInDoors", function()
+	if CurTime() > delay then 
+		DaMachine()
 
 		if not impulse.GetSetting("hud_ambience") then
 			hook.Remove("RenderScreenspaceEffects", "HatchetAmbienceColor")
 		end
-
-		if IsIndoors() == false then
-			return
-		else
-			return
-		end
-		
-		delay = CurTime() + 3
+		delay = CurTime() + 1
 	end
 end)
+
+
+
+
+/*
+	if not impulse.GetSetting("hud_ambience") then
+		hook.Remove("RenderScreenspaceEffects", "HatchetAmbienceColor")
+	end
