ce9e96a8678f94f4ee678979484fe6cea9583ddb
ForWhomTheBeatTolls <155327198+ForWhomTheBeatTolls@users.noreply.github.com>
2024-06-10 18:39:42 +0300

    Update ls_shovel.lua

--- gamemodes/skeleton-schema/entities/weapons/ls_shovel.lua
+++ gamemodes/skeleton-schema/entities/weapons/ls_shovel.lua
@@ -28,6 +28,7 @@ SWEP.Primary.Recoil = 1.4 -- base recoil value, SWEP.Spread mods can change this
 SWEP.Primary.Damage = 30 -- not used in this swep
 SWEP.Primary.NumShots = 1
 SWEP.Primary.HitDelay = 0.3
+SWEP.Primary.HullSize = 12
 SWEP.Primary.Delay = 1.2
 SWEP.Primary.Range = 90
 SWEP.Primary.StunTime = 0.4
