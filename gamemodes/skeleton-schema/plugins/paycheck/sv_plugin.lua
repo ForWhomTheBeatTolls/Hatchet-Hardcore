f91017ab17eaa4f21b447e4dbe6603f7eed735bc
ForWhomTheBeatTolls <155327198+ForWhomTheBeatTolls@users.noreply.github.com>
2024-06-10 22:29:14 +0300

    Fixed paychecks

--- gamemodes/skeleton-schema/plugins/paycheck/sv_plugin.lua
+++ gamemodes/skeleton-schema/plugins/paycheck/sv_plugin.lua
@@ -1,17 +1,17 @@
-local paycheckdelay = CurTime() + 10
+local paycheckdelay = CurTime() + 120
 
 function PLUGIN:Think()
-	if CurTime() > paycheckdelay then
-		for k,v in pairs(player.GetAll()) do
-			if v:Team() == TEAM_CITIZEN then
-			v:GiveBankMoney(10)
-			elseif v:Team() == TEAM_CP then
-			v:GiveBankMoney(30)
-			elseif v:Team() == TEAM_RESISTANCE then
-			v:GiveBankMoney(1)
+		if CurTime() > paycheckdelay then
+			for k,v in pairs(player.GetAll()) do
+				if v:Team() == TEAM_CITIZEN then
+				v:GiveBankMoney(10)
+				elseif v:Team() == TEAM_CP then
+				v:GiveBankMoney(30)
+				elseif v:Team() == TEAM_RESISTANCE then
+				v:GiveBankMoney(1)
+			end
 		end
+		paycheckdelay = CurTime() + 120
 	end
-	paycheckdelay = CurTime() + 10
 end
-			
 			
