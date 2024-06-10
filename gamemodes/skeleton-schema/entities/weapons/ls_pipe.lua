ec1b6a9339dea716574efc21bfc4f73bf986b9a3
ForWhomTheBeatTolls <155327198+ForWhomTheBeatTolls@users.noreply.github.com>
2024-06-10 18:39:31 +0300

    Update ls_pipe.lua

--- gamemodes/skeleton-schema/entities/weapons/ls_pipe.lua
+++ gamemodes/skeleton-schema/entities/weapons/ls_pipe.lua
@@ -28,6 +28,7 @@ SWEP.Primary.Recoil = 1.4 -- base recoil value, SWEP.Spread mods can change this
 SWEP.Primary.Damage = 25 -- not used in this swep
 SWEP.Primary.NumShots = 1
 SWEP.Primary.HitDelay = 0.3
+SWEP.Primary.HullSize = 12
 SWEP.Primary.Delay = 1
 SWEP.Primary.Range = 80
 SWEP.Primary.StunTime = 0.3
