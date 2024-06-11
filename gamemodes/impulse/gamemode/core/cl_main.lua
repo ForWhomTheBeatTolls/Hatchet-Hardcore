02486ed7583c7f7ee9dfcef74ee1f2eafeac258b
WillMaster <79755822+willmasterLUA@users.noreply.github.com>
2024-06-11 17:36:51 +0200

    Update cl_main.lua

--- gamemodes/impulse/gamemode/core/cl_main.lua
+++ gamemodes/impulse/gamemode/core/cl_main.lua
@@ -20,6 +20,30 @@ local superTesters = {
 	["STEAM_0:1:175014750"] = false -- personwhoplaysgames
 }
 
+local hatchet_mappers = {
+	["STEAM_0:0:36683492"] = true -- stranger
+}
+
+local hatchet_contributors = {
+	[""] = true -- stranger
+}
+
+local hatchet_majorcontributors = {
+	["STEAM_0:0:504346304"] = true -- TehRedd
+}
+
+local hatchet_leaddev = {
+	["STEAM_0:0:193471001"] = true -- WillMaster
+}
+
+local hatchet_dev = {
+	["STEAM_0:0:627119036"] = true -- stranger
+}
+
+local hatchet_projectlead = {
+	["STEAM_0:0:193471001"] = true -- WillMaster
+}
+
 local mappers = {
 	["STEAM_0:0:24607430"] = false -- stranger
 }
@@ -33,12 +57,18 @@ local winners = {}
 
 -- Please don't ever remove credit or users/badges from this section. People worked hard on this. Thanks!
 impulse.Badges = {
-	staff = {Material("icon16/shield.png"), "This player is a staff member.", function(ply) return not ply:IsIncognito() and ply:IsAdmin() end},
+	staff = {Material("icon16/shield.png"), "This player is part of the Janitor team. (Staff)", function(ply) return not ply:IsIncognito() and ply:IsAdmin() end},
 	donator = {Material("icon16/coins.png"), "This player is a donator.", function(ply) return ply:IsDonator() end},
 	exdev = {Material("icon16/cog_go.png"), "This player is a ex impulse developer.", function(ply) return ply:SteamID() == "STEAM_0:1:102639297" end},
 	dev = {Material("icon16/cog.png"), "This player is a impulse developer.", function(ply) return not ply:IsIncognito() and ply:IsDeveloper() end},
 	vin = {Material("decals/lambdaspray_2a"), "Hello. I'm the creator of Hatchet HL2RPG.", function(ply) return not ply:IsIncognito() and (ply:SteamID() == "STEAM_0:1:204367223") end},
-	supertester = {Material("icon16/bug.png"), "This player made large contributions to the testing of impulse.", function(ply) return (superTesters[ply:SteamID()] or false) end},
+	hatchetmapper = {Material("icon16/building_edit.png"), "This player is a mapper for Hatchet HL2RPG.", function(ply) return not ply:IsIncognito() and (hatchet_mappers[ply:SteamID()] or false) end},
+	hatchetdev = {Material("icon16/cog.png"), "This player is a junior developer for Hatchet HL2RPG.", function(ply) return not ply:IsIncognito() and (hatchet_dev[ply:SteamID()] or false) end},
+	hatchetleaddev = {Material("icon16/application_osx_terminal.png"), "This player is a Lead developer for Hatchet HL2RPG.", function(ply) return not ply:IsIncognito() and (hatchet_leaddev[ply:SteamID()] or false) end},
+	hatchetcontributor = {Material("icon16/award_star_bronze_1.png"), "This Player contributed on Hatchet HL2RPG.", function(ply) return not ply:IsIncognito() and (hatchet_contributors[ply:SteamID()] or false) end},
+	hatchetmajorcontributor = {Material("icon16/award_star_gold_3.png"), "This Player did Major contribution on Hatchet HL2RPG.", function(ply) return not ply:IsIncognito() and (hatchet_majorcontributors[ply:SteamID()] or false) end},
+	hatchetprojectlead = {Material("icon16/application_osx.png"), "This player is a project leader for Hatchet HL2RPG.", function(ply) return not ply:IsIncognito() and (hatchet_projectlead[ply:SteamID()] or false) end},
+	supertester = {Material("icon16/bug.png"), "This player made large contributions to the testing of impulse.", function(ply) return not ply:IsIncognito() and (superTesters[ply:SteamID()] or false) end},
 	competition = {Material("icon16/rosette.png"), "This player has won a competition.", function(ply) return winners[ply:SteamID()] end},
 	mapper = {Material("icon16/map.png"), "This player is a mapper that has collaborated with impulse.", function(ply) return mappers[ply:SteamID()] end},
 	eventteam = {Material("icon16/controller.png"), "This player is the leader of the event team.", function(ply) return eventTeam[ply:SteamID()] end},
