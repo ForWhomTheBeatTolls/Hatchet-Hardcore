4463ce1a244b65a4201ee424492d1c1406556404
ForWhomTheBeatTolls <155327198+ForWhomTheBeatTolls@users.noreply.github.com>
2024-06-11 21:39:25 +0300

    Workforce puzzle

--- gamemodes/skeleton-schema/schema/scripts/vgui/cl_workforcepuzzle1.lua
+++ gamemodes/skeleton-schema/schema/scripts/vgui/cl_workforcepuzzle1.lua
@@ -2,10 +2,13 @@ local PANEL = {}
 
 impulse.PuzzleWorkforce1 = impulse.PuzzleWorkforce1 or nil
 
+local success
+local nextclickdecay = CurTime() + 5
+local endtime
 function PANEL:Init()
 
-	// Looking to steal this clientside code?
-	// dont be like Kazootoa :-)
+	// Looking to steal this clientside code? It would have been munch simpler to ask. FUCK YOU!
+	// dont be like Kazotoa :-)
 
 	if impulse.PuzzleWorkforce1 and IsValid(impulse.PuzzleWorkforce1) then
 		impulse.PuzzleWorkforce1:Remove()
@@ -20,8 +23,16 @@ function PANEL:Init()
 	self:SetSize(1200, 140)
 	self:SetTitle("Minigame")
 	self:MakePopup()
+	
+	pzlwrkfc1_clickometer = vgui.Create( "DShape", self )
+	pzlwrkfc1_clickometer:SetType( "Rect" ) -- This is the only type it can be
+	pzlwrkfc1_clickometer:SetPos( 0, 24 ) 
+	pzlwrkfc1_clickometer:SetColor( Color(255, 165, 0, 255) )
+	pzlwrkfc1_starttime = CurTime()
+	
 end
 
+local x = 0
 function PANEL:Paint(w, h)
 
 	local barposheight = 24
@@ -30,38 +41,77 @@ function PANEL:Paint(w, h)
 
 
 
-	// point box's
+	// clickometer 
+	
+	pzlwrkfc1_clickometer:SetSize( x, 100 )
 
-	surface.SetDrawColor(255, 94, 0)
-	surface.DrawRect(310, barposheight, 600, 100)
-
-	surface.SetDrawColor(255, 217, 0)
-	surface.DrawRect(390, barposheight, 450, 100)
-
-	surface.SetDrawColor(31, 117, 247)
-	surface.DrawRect(490, barposheight, 240, 100)
-
-	surface.SetDrawColor(26, 88, 6)
-	surface.DrawRect(555, barposheight, 110, 100)
-
-	//line
-
-	local speed = 0.1
-	local range = 1
-	local offset = range * math.sin(CurTime() * speed)
-	local animation = Lerp(0, 50, 555)
-
-
-	surface.SetDrawColor(255, 0, 0)
-	surface.DrawRect(w * offset, barposheight, 6, 100)
 
 	// base
-	
+
 	surface.SetDrawColor(0, 0, 0)
 	surface.DrawOutlinedRect(-4, barposheight, w * 2, 100, 2)
 
 	//draw.RoundedBox(2, 0, 0, w, h, Color(219, 214, 135))
 	//draw.RoundedBox(1, 0, 0, w, h - 574, Color(0, 4, 255))
 end
+
+local increment = 32
+local clickamount = 0
+function PANEL:OnMousePressed(MOUSE_LEFT)
+	if x < 1200 then
+		x = x + increment
+		clickamount = clickamount + 1
+		nextclickdecay = CurTime() + 1
+		--pzlwrkfc1_clickometer:SetSize( x, 100 )
+		print(x)
+		print(clickamount)
+	else
+		success = true
+		print(success)
+		LocalPlayer():Notify("You did it! CPS: "..math.Truncate(clickamount / (CurTime() - pzlwrkfc1_starttime), 1))
+		nextclickdecay = CurTime() + 1
+		clickamount = 0
+		self:Close()
+	end
+end
+
+function PANEL:Think()
+	pzlwrkfc1_clickometer:SetSize( x, 100 )
+	if nextclickdecay != nil and nextclickdecay < CurTime() then
+		--clickamount = clickamount - 1
+		x = x - increment
+		--pzlwrkfc1_clickometer:SetSize( x, 100 )
+		nextclickdecay = CurTime() + 1.5
+		
+		if x < 0 then
+			nextclickdecay = nil
+			x = 0
+			LocalPlayer():Notify("You have failed.")
+			success = false
+			print(success)
+			self:Close()
+		end
+		
+	end
+end
+
+function PANEL:OnRemove()
+	if success == true then
+		nextclickdecay = nil
+		x = 0
+	else
+		LocalPlayer():Notify("You have failed.")
+		success = false
+	end
+end
+
+
+
+
+
+
+
+
+
 
 vgui.Register("impulseWorkforcePuzzle1", PANEL, "DFrame")
