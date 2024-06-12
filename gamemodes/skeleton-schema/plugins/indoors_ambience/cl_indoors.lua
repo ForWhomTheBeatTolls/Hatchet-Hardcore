b68e94e3ee2f6c84a423e94075511b77c4535fa7
WillMaster <79755822+willmasterLUA@users.noreply.github.com>
2024-06-12 23:42:38 +0200

    make ambience sounds and automatic ambience color tooglable

--- gamemodes/skeleton-schema/plugins/indoors_ambience/cl_indoors.lua
+++ gamemodes/skeleton-schema/plugins/indoors_ambience/cl_indoors.lua
@@ -31,7 +31,9 @@ function DaMachine()
 	IsIndoors()
 
 	if indoor then
-		day:PlayEx(0.17, 60)
+		if impulse.GetSetting("ambience_sounds") then
+			day:PlayEx(0.17, 60)
+		end
 		if not doneItOnce then
 			doneItOnce = true
 			if impulse.GetSetting("hud_ambience") then
@@ -44,10 +46,14 @@ function DaMachine()
 	end
 
 	if not indoor then
-		day:Stop()
+		if impulse.GetSetting("ambience_sounds") then
+			day:Stop()
+		end
 		if doneItOnce then
 			doneItOnce = false
-			hook.Remove("RenderScreenspaceEffects", "HatchetAmbienceColor")
+			if impulse.GetSetting("hud_ambienceindoors") then
+				hook.Remove("RenderScreenspaceEffects", "HatchetAmbienceColor")
+			end
 			//print("Inside")
 		end
 	end
