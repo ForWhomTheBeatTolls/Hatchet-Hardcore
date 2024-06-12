ef9e5f2796263d49c5aea035b7a05068142935fb
ForWhomTheBeatTolls <155327198+ForWhomTheBeatTolls@users.noreply.github.com>
2024-06-12 14:39:28 +0300

    Update sh_hudselectioncore.lua

--- gamemodes/skeleton-schema/plugins/Selective_Huds/sh_hudselectioncore.lua
+++ gamemodes/skeleton-schema/plugins/Selective_Huds/sh_hudselectioncore.lua
@@ -1,6 +1,9 @@
 AddCSLuaFile("cl_huds.lua")
 
+if CLIENT then
+
 impulse.DefineSetting("crosshair_radius", {name="Crosshair Radius", category="HUD", type="slider", default=8, minValue=1, maxValue=40})
 impulse.DefineSetting("hud_selection", {name="HUD Design", category="HUD", type="dropdown", default="Hatchet", options={"Hatchet", "Hatchet Legacy"}})
 impulse.DefineSetting("crosshair_selection", {name="Crosshair Design", category="HUD", type="dropdown", default="Default", options={"Default", "Hatchet Legacy", "Half-Life 2"}})
 
+end
