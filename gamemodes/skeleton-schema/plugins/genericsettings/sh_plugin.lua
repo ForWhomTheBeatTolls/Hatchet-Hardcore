d4b1106bc8766a1f65730240c8e809a6c38518a4
ForWhomTheBeatTolls <155327198+ForWhomTheBeatTolls@users.noreply.github.com>
2024-06-12 14:37:43 +0300

    Update sh_plugin.lua

--- gamemodes/skeleton-schema/plugins/genericsettings/sh_plugin.lua
+++ gamemodes/skeleton-schema/plugins/genericsettings/sh_plugin.lua
@@ -1,4 +1,6 @@
+if CLIENT then
 impulse.DefineSetting("hud_jim", {name="Movement Indicator", category="HUD", type="tickbox", default=false})
 impulse.DefineSetting("hud_hunger", {name="Hunger Bar", category="HUD", type="tickbox", default=true})
 impulse.DefineSetting("hud_ambience", {name="Ambience Color", category="HUD", type="tickbox", default=true})
 impulse.DefineSetting("admin_dispatch", {name="Dispatch", category="ops", type="tickbox", default=false})
+end
