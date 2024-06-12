72a7f1db38f355be7c5e4673dc365eef16aaa3c9
ForWhomTheBeatTolls <155327198+ForWhomTheBeatTolls@users.noreply.github.com>
2024-06-12 14:02:50 +0300

    Update workforce locker

--- gamemodes/skeleton-schema/entities/entities/impulse_workforce_locker/init.lua
+++ gamemodes/skeleton-schema/entities/entities/impulse_workforce_locker/init.lua
@@ -39,7 +39,7 @@ function ENT:SpawnFunction(ply, trace, class)
 end
 
 function ENT:Use(activator, caller)
-	if activator:IsPlayer() and activator:Alive() and activator:Team() == TEAM_WRKFORCE then
+	if activator:IsPlayer() and activator:Alive() and activator:Team() == TEAM_WORKFORCE then
 		if activator:GetTeamRank() == RANK_MEDICAL then
 			activator:GiveInventoryItem("clothing_medicalshirt", 1, true )
 		elseif activator:GetTeamRank() == RANK_INDUSTRIAL then
