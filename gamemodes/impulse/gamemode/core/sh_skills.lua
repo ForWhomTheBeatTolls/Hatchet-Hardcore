ac60e5293229151ef18a07baad47df27a734d1ae
WillMaster <79755822+willmasterLUA@users.noreply.github.com>
2024-06-13 13:53:14 +0200

    Remove "(DEV") From Skill Name

--- gamemodes/impulse/gamemode/core/sh_skills.lua
+++ gamemodes/impulse/gamemode/core/sh_skills.lua
@@ -47,7 +47,7 @@ function meta:GetSkillLevel(name)
 	return 10
 end
 
-impulse.Skills.Define("vital", "Vitality (Dev)")
+impulse.Skills.Define("vital", "Vitality")
 impulse.Skills.Define("craft", "Crafting")
 --impulse.Skills.Define("medicine", "Medicine")
 impulse.Skills.Define("strength", "Strength")
