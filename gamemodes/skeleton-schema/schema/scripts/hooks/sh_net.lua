8a9541ecd7a66fb1b940128c93a24738720d1501
WillMaster <79755822+willmasterLUA@users.noreply.github.com>
2024-06-14 19:35:28 +0200

    Dist check on bubblechat

--- gamemodes/skeleton-schema/schema/scripts/hooks/sh_net.lua
+++ gamemodes/skeleton-schema/schema/scripts/hooks/sh_net.lua
@@ -7,14 +7,52 @@ net.Receive("HatchetBubbleChatCall", function()
     local ply = net.ReadPlayer()
     local sender = Entity(ply:EntIndex())
 
-    local textt = msg
-
     local textappear = true
 
+    // This is fucking 100% cancerous dude, i swear this is like the devil of all coding.
+    // If you're a code stealer and want to steal this code, Please dont, not that its because we worked hard on it, but its more that
+    // we hope you will not follow this method of coding.
+    // (also fuck you if ur gonna use this by stealing)
+
+    // I hope there will be a better way
+
+    if string.StartsWith(msg, "/ac") then
+        msg = ""
+    elseif string.StartsWith(msg, "/ooc") then
+        msg = ""
+    elseif string.StartsWith(msg, "/looc") then
+        msg = ""
+    elseif string.StartsWith(msg, "//.") then
+        msg = ""
+    elseif string.StartsWith(msg, "//") then
+        msg = ""
+    elseif string.StartsWith(msg, "/event") then
+        msg = ""
+    elseif string.StartsWith(msg, "/itemspawner") then
+        msg = ""
+    elseif string.StartsWith(msg, "/bring") then
+        msg = ""
+    elseif string.StartsWith(msg, "/goto") then
+        msg = ""
+    elseif string.StartsWith(msg, "/giveitem") then
+        msg = ""
+    elseif string.StartsWith(msg, "/kick") then
+        msg = ""
+    elseif string.StartsWith(msg, "/ban") then
+        msg = ""
+    end
+
+    //i want to kill myself with a shotgun.
+
     hook.Add("HUDPaint", ply:SteamID().."_OverheadChatHUD", function()
-        local pos = (sender:GetBonePosition(sender:LookupBone("ValveBiped.Bip01_Pelvis")) + sender:OBBCenter()):ToScreen()
-        if textappear then
-            draw.SimpleTextOutlined(textt, font, pos.x, pos.y, clr, TEXT_ALIGN_CENTER, nil, 1, Color(0, 0, 0))
+        if ( sender:IsValid() and sender:Alive() and sender:GetMoveType() != MOVETYPE_NOCLIP and sender:IsEffectActive(EF_NODRAW) != true ) then
+            local pos = (sender:GetBonePosition(sender:LookupBone("ValveBiped.Bip01_Spine1")) + sender:OBBCenter()):ToScreen()
+            local posdist = sender:GetPos()
+            if sender:IsValid() and LocalPlayer():GetPos():Distance( posdist ) <= 418 then
+                if textappear then
+                    draw.SimpleTextOutlined(msg, font, pos.x, pos.y, clr, TEXT_ALIGN_CENTER, nil, 1, Color(0, 0, 0))
+                end
+            end
         end
     end)
 
