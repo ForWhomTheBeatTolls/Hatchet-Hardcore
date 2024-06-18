532b963455f4660e1917cf76c9ee5c4ff01dd2b8
ForWhomTheBeatTolls <155327198+ForWhomTheBeatTolls@users.noreply.github.com>
2024-06-18 20:36:10 +0300

    Add missing parentheses

--- gamemodes/skeleton-schema/schema/scripts/hooks/sh_net.lua
+++ gamemodes/skeleton-schema/schema/scripts/hooks/sh_net.lua
@@ -14,7 +14,7 @@ net.Receive("HatchetBubbleChatCall", function()
     local nextTime = 0
 
     hook.Add("HUDPaint", ply:SteamID().."_OverheadChatHUD", function()
-        if ( sender:IsValid() and sender:Alive() and sender:GetMoveType() != MOVETYPE_NOCLIP then
+        if ( sender:IsValid() and sender:Alive() and (sender:GetMoveType() != MOVETYPE_NOCLIP) ) then
             local pos = (sender:GetBonePosition(sender:LookupBone("ValveBiped.Bip01_Spine1")) + sender:OBBCenter()):ToScreen()
             local posdist = sender:GetPos()
             if sender:IsValid() and LocalPlayer():GetPos():Distance( posdist ) <= chatradius and LocalPlayer():IsLineOfSightClear(sender) then
