9d59b60cc36085602cb38d0ceae5d527a5ece7d9
ForWhomTheBeatTolls <155327198+ForWhomTheBeatTolls@users.noreply.github.com>
2024-06-18 16:54:58 +0300

    Vehicle anims fix pt. 1

--- gamemodes/impulse/gamemode/core/sh_anims.lua
+++ gamemodes/impulse/gamemode/core/sh_anims.lua
@@ -100,9 +100,9 @@ impulse.Anim.citizen_male = {
 	--talk = LocalPlayer():GetSequenceList(),
 	vehicle = {
 		["prop_vehicle_prisoner_pod"] = {"podpose", Vector(-3, 0, 0)},
-		["prop_vehicle_jeep"] = {ACT_BUSY_SIT_CHAIR, Vector(14, 0, -14)},
-		["prop_vehicle_airboat"] = {ACT_BUSY_SIT_CHAIR, Vector(8, 0, -20)},
-		chair = {ACT_BUSY_SIT_CHAIR, Vector(1, 0, -23)}
+		["prop_vehicle_jeep"] = {ACT_BUSY_SIT_CHAIR, Vector(0, 0, 0)},
+		["prop_vehicle_airboat"] = {ACT_BUSY_SIT_CHAIR, Vector(0, 0, 0)},
+		chair = {ACT_BUSY_SIT_CHAIR, Vector(0, 0, 0)}
 	},
 }
 
