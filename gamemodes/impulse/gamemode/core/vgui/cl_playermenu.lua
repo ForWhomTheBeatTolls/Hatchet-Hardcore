3e98407d9e5d2eb112de04e2656d40e3499e0b61
WillMaster <79755822+willmasterLUA@users.noreply.github.com>
2024-06-19 00:58:54 +0200

    Update cl_playermenu.lua

--- gamemodes/impulse/gamemode/core/vgui/cl_playermenu.lua
+++ gamemodes/impulse/gamemode/core/vgui/cl_playermenu.lua
@@ -43,6 +43,7 @@ function PANEL:Init()
 	local defaultButton = self:AddSheet("Actions", Material("impulse/icons/banknotes-256.png"), self.quickActions, self.QuickActions)
 	self:AddSheet("Information", Material("impulse/icons/info-256.png"), self.info, self.Info)
 	self:AddSheet("Business", Material("impulse/icons/cart-73-256.png"), self.business, self.Business) // keeping this for now
+	//self:AddSheet("Teams", Material("impulse/icons/group-256.png"), self.teams, self.Teams)
 
 	self.tabSheet:SetActiveButton(defaultButton)
 	defaultButton.loaded = true
