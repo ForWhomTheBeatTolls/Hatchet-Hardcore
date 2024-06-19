f1695c829731ebe690c949a2d61fdf8803d0a739
WillMaster <79755822+willmasterLUA@users.noreply.github.com>
2024-06-19 13:59:32 +0200

    description system part1

--- gamemodes/impulse/gamemode/core/sv_sql.lua
+++ gamemodes/impulse/gamemode/core/sv_sql.lua
@@ -4,6 +4,7 @@ function GM:DatabaseConnected()
     local sqlQuery = mysql:Create("impulse_players") -- if not table exist, make it.
         sqlQuery:Create("id", "int unsigned NOT NULL AUTO_INCREMENT") -- index
         sqlQuery:Create("rpname", "varchar(70) NOT NULL") -- rpname
+        sqlQuery:Create("rpdesc", "varchar(255) NOT NULL") -- rpdesc
         sqlQuery:Create("steamid", "varchar(25) NOT NULL") -- steamid
         sqlQuery:Create("group", "varchar(70) NOT NULL") -- usergroup
         sqlQuery:Create("rpgroup", "int(11) unsigned NOT NULL") -- rpgroup
