5108f272890b97b48cea5fcdd82d2de0ab2d81b5
WillMaster <79755822+willmasterLUA@users.noreply.github.com>
2024-06-19 16:36:37 +0200

    Update sh_b_resistance.lua

--- gamemodes/skeleton-schema/schema/teams/sh_b_resistance.lua
+++ gamemodes/skeleton-schema/schema/teams/sh_b_resistance.lua
@@ -7,5 +7,54 @@ TEAM_RESISTANCE = impulse.Teams.Define({
 	limit = 0,
 	xp = 10,
 	cp = false,
-	donatorOnly = false -- set this to true for donatorOnly
+	classes = {
+		{
+			name = "Refugee",
+			description = "temp desc",
+			xp = 0,
+			noMenu = true,
+			onBecome = function(ply)
+				--ply:SetModel("models/Police.mdl")
+			end
+		},
+		{
+			name = "Rebel",
+			description = "temp desc",
+			xp = 0,
+			noMenu = true,
+			onBecome = function(ply)
+				--ply:SetModel("models/Police.mdl")
+			end
+		},
+	},
+
+
+	ranks = {
+		{
+			name = "Runner",
+			description = "temp desc",
+			xp = 0,
+		},
+		{
+			name = "Medical",
+			description = "temp desc",
+			xp = 0,
+		},
+		{
+			name = "Engineer",
+			description = "temp desc",
+			--itemsAdd = {
+			--	{class = "wep_pistol", amount = 1}
+			--},
+			doorGroup = {1, 2},
+			xp = 0,
+		},
+	},
 })
+
+CLASS_REFUGEE = 1
+CLASS_REBEL = 1
+
+RANK_RUNNER = 1
+RANK_MEDICAL = 2
+RANK_ENGINEER = 3
