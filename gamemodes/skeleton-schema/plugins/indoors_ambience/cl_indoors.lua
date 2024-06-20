88f1dc50f7552f6e9ab20502b8b4ed236a8bf980
WillMaster <79755822+willmasterLUA@users.noreply.github.com>
2024-06-20 17:27:14 +0200

    Update cl_indoors.lua

--- gamemodes/skeleton-schema/plugins/indoors_ambience/cl_indoors.lua
+++ gamemodes/skeleton-schema/plugins/indoors_ambience/cl_indoors.lua
@@ -17,15 +17,15 @@ function DaMachine()
 
 	local day = CreateSound(LocalPlayer(), "ambient/forest_day.wav")
 	local ambience = {
-	["$pp_colour_addr"] = 0,
-	["$pp_colour_addg"] = 0,
-	["$pp_colour_addb"] = 0,
-	["$pp_colour_brightness"] = -0.04,
-	["$pp_colour_contrast"] = 1.4,
-	["$pp_colour_colour"] = 0.8,
-	["$pp_colour_mulr"] = 0,
-	["$pp_colour_mulg"] = 0,
-	["$pp_colour_mulb"] = 0
+		["$pp_colour_addr"] = 0,
+		["$pp_colour_addg"] = 0,
+		["$pp_colour_addb"] = 0,
+		["$pp_colour_brightness"] = -0.04,
+		["$pp_colour_contrast"] = 1.5,
+		["$pp_colour_colour"] = 0.8,
+		["$pp_colour_mulr"] = 0,
+		["$pp_colour_mulg"] = 0,
+		["$pp_colour_mulb"] = 0
 	}
 
 	IsIndoors()
