0066f08dd4da2c39e12ce13c0c0a073da5b42e79
ForWhomTheBeatTolls <155327198+ForWhomTheBeatTolls@users.noreply.github.com>
2024-06-20 15:51:30 +0300

    Create cl_hudselectioncore.lua

---
+++ gamemodes/skeleton-schema/schema/scripts/cl_hudselectioncore.lua
@@ -0,0 +1,10 @@
+AddCSLuaFile("cl_huds.lua")
+
+if CLIENT then
+
+impulse.DefineSetting("crosshair_radius", {name="Crosshair Radius", category="HUD", type="slider", default=8, minValue=1, maxValue=40})
+impulse.DefineSetting("hud_selection", {name="HUD Design", category="HUD", type="dropdown", default="Hatchet", options={"Hatchet", "Hatchet Legacy", "Axe"}})
+impulse.DefineSetting("crosshair_selection", {name="Crosshair Design", category="HUD", type="dropdown", default="Default", options={"Default", "Hatchet Legacy", "Half-Life 2", "Circle + Dot"}})
+impulse.DefineSetting("crosshair_color", {name="Crosshair Color", category="HUD", type="dropdown", default="White", options={"White", "Green", "Yellow", "Red", "Blue", "Purple", "Orange"}})
+
+end
