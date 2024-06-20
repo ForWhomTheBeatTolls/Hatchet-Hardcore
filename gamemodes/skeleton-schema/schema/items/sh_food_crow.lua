fbe77954c44e9e8c50e6bbabf04e10275aad642f
ForWhomTheBeatTolls <155327198+ForWhomTheBeatTolls@users.noreply.github.com>
2024-06-20 23:06:07 +0300

    Increase food poisoning time

--- gamemodes/skeleton-schema/schema/items/sh_food_crow.lua
+++ gamemodes/skeleton-schema/schema/items/sh_food_crow.lua
@@ -36,8 +36,8 @@ function ITEM:OnUse(ply)
 		
 		if timer.Exists(ply:EntIndex().."FoodPoisoning") then timer.Remove(ply:EntIndex().."FoodPoisoning") end
 			
-		timer.Create(ply:EntIndex().."FoodPoisoning", 5, 30, function()
-			if ply:Alive() then
+		timer.Create(ply:EntIndex().."FoodPoisoning", 30, 30, function()
+			if ply:Alive() and ply.FoodPoisoning == true then
 				dmg:SetDamage(math.random(0,2))
 				dmg:SetDamageType( DMG_NERVEGAS )
 				ply:TakeDamageInfo(dmg)
