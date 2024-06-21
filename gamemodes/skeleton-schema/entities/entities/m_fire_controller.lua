a66926d2dbf1945a677d661fb941151d9bbf3db1
ForWhomTheBeatTolls <155327198+ForWhomTheBeatTolls@users.noreply.github.com>
2024-06-21 19:04:04 +0300

    Careful... don't let the lazy code fuck up your performance!

--- gamemodes/skeleton-schema/entities/entities/m_fire_controller.lua
+++ gamemodes/skeleton-schema/entities/entities/m_fire_controller.lua
@@ -18,6 +18,9 @@ self:SetColor( Color( 255, 255, 255, 0 ) )
 self:SetMoveType( MOVETYPE_VPHYSICS )
 self:SetSolid( SOLID_VPHYSICS )
 self:SetCollisionGroup(COLLISION_GROUP_DEBRIS)
+self.nextfiresound = CurTime()
+self.nextfiretick = CurTime() + 1
+self.cooksoundplayed = false
 
 local fire = ents.Create("env_fire")
 
@@ -41,42 +44,68 @@ end
 
 end
 
-local nextfiretick = CurTime() + 1
 function ENT:Think()
 
 	if SERVER then
+	
+	if self.nextfiresound < CurTime() then
+		self:StopSound("ambient/fire/fire_small1.wav")
+		self:EmitSound("ambient/fire/fire_small1.wav", 60)
+		self.nextfiresound = CurTime() + 5
+	end
 	
 	for k,v in pairs(self:GetChildren()) do
 		v:SetPos(self:GetPos())
 	end
 	
 	--if nextfiretick < CurTime() then
-		for k,v in pairs(ents.FindByClass("impulse_item")) do
+		for k,v in pairs(ents.FindInCone(self:GetPos() + Vector(0,0,8), Vector(0,0,90), 25, 0)) do
 		local firerange = 1050
 		local DistToFire = v:GetPos():DistToSqr(self:GetPos())
 			if DistToFire < firerange then
-				local itemid = v:GetItemID()
-				local item = impulse.Inventory.Items[itemid]
-				local vpos = v:GetPos()
-				if item.Name == "Can of Water" then
-					timer.Simple(5, function() if IsValid(v) and DistToFire < firerange then local fishpos = v:GetPos() v:Remove() impulse.Inventory.SpawnItem("util_scrapmetal", fishpos) end end)
-				elseif item.Name == "Raw Fish" then
-					timer.Simple(5, function() if IsValid(v) and DistToFire < firerange then local fishpos = v:GetPos() v:Remove() impulse.Inventory.SpawnItem("food_cookedfish", fishpos + Vector(0,0,1)) end end)
-				elseif item.Name == "Dead Crow" then
-					timer.Simple(5, function() if IsValid(v) and DistToFire < firerange then local fishpos = v:GetPos() v:Remove() impulse.Inventory.SpawnItem("food_cookedcrow", fishpos + Vector(0,0,1)) end end)
-				else
-					if nextfiretick < CurTime() then
-						v:TakeDamage(1, self, nil)
+				if v:GetClass() == "impulse_item" then
+					local itemid = v:GetItemID()
+					local item = impulse.Inventory.Items[itemid]
+					local vpos = v:GetPos()
+					local f,r,u = v:GetForward(), v:GetRight(), v:GetUp()
+					if item.Name == "Can of Water" then
+						timer.Simple(5, function() if IsValid(v) and DistToFire < firerange then local fishpos = v:GetPos() v:Remove() impulse.Inventory.SpawnItem("util_scrapmetal", fishpos) end end)
+					elseif item.Name == "Raw Fish" then
+						timer.Simple(5, function() if IsValid(v) and DistToFire < firerange then local fishpos = (v:GetPos() + Vector(0,0,15)) v:Remove() impulse.Inventory.SpawnItem("food_cookedfish", fishpos) end end)
+					elseif item.Name == "Dead Crow" then
+						timer.Simple(5, function() if IsValid(v) and DistToFire < firerange then local fishpos = (v:GetPos() + Vector(0,0,15)) v:Remove() impulse.Inventory.SpawnItem("food_cookedcrow", fishpos) end end)
+					else
+						if self.nextfiretick < CurTime() then
+							v:TakeDamage(1, self, nil)
+							self:EmitSound("ambient/fire/gascan_ignite1.wav", 40)
+						end
+						self.nextfiretick = CurTime() + 1
 					end
-					--print(item.Name)
-					nextfiretick = CurTime() + 1
-					--print(v:GetItemName())
+				elseif v:IsPlayer() then
+					if self.nextfiretick < CurTime() then
+						if math.random(1, 100) >= 60 then
+							v:Ignite(10, 20)
+						end
+						v:TakeDamage(1, self, nil)
+						v:EmitSound("ambient/fire/gascan_ignite1.wav", 40)
+						v:EmitSound("player/pl_burnpain1.wav", 70)
+						self.nextfiretick = CurTime() + 1
+					end
+				else
+					if self.nextfiretick < CurTime() then
+						if math.random(1, 100) >= 60 then
+							v:Ignite(10, 20)
+						end
+						v:TakeDamage(1, self, nil)
+						v:EmitSound("ambient/fire/gascan_ignite1.wav", 40)
+						self.nextfiretick = CurTime() + 1
+					end
 				end
-			end
+			end	
 		end
-		--nextfiretick = CurTime() + 1
-	--end
-	
 	end
-	
+end
+
+function ENT:OnRemove()
+	self:StopSound("ambient/fire/fire_small1.wav")
 end
