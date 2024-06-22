b7b22c255e5a7ab564847afb906cd4392ba3ffa6
ForWhomTheBeatTolls <155327198+ForWhomTheBeatTolls@users.noreply.github.com>
2024-06-22 18:42:50 +0300

    Update ls_stunstick.lua

--- gamemodes/skeleton-schema/entities/weapons/ls_stunstick.lua
+++ gamemodes/skeleton-schema/entities/weapons/ls_stunstick.lua
@@ -75,9 +75,11 @@ function SWEP:PrePrimaryAttack()
 	else
 		if mode == 2 then
 			self.Primary.Damage = 6
+			self.Primary.DoStun = true
 			self.Primary.FlashTime = 0.8
 		else
 			self.Primary.Damage = 10
+			self.Primary.DoStun = true
 			self.Primary.FlashTime = 1.1
 		end
 
@@ -85,6 +87,155 @@ function SWEP:PrePrimaryAttack()
 		self.Primary.Sound = Sound("weapons/stunstick/stunstick_swing1.wav")
 		self.Primary.ImpactSound = Sound("weapons/stunstick/stunstick_impact2.wav")
 	end
+end
+
+function SWEP:PrimaryAttack()
+	local boxSize = self.Primary.HullSize
+	if self.PrePrimaryAttack then
+		self.PrePrimaryAttack(self)
+	end
+
+	if self.Primary.HitDelay then
+		timer.Simple(self.Primary.HitDelay, function()
+			if IsValid(self) and IsValid(self.Owner) then
+				if SERVER then
+				local bullet = {}
+				bullet.Num    = 1
+				bullet.AmmoType = "Snark"
+				bullet.Src    = self.Owner:GetShootPos()
+				bullet.Dir    = self.Owner:GetAimVector()
+				bullet.Spread = Vector(0, 0, 0)
+				bullet.Tracer = 0
+				bullet.Force  = 0
+				bullet.Hullsize = self.Primary.HullSize
+				bullet.Distance = self.Primary.Range + (self.Owner:GetVelocity():LengthSqr() / 950)
+				bullet.Damage = self.Primary.Damage
+				bullet.Callback = function(attacker, tr, dmginfo)
+					if tr.Hit then
+					
+					--debugoverlay.Cross(tr.HitPos, 2, 3, Color(255, 0, 0), true)
+					--debugoverlay.Cross(tr.Entity:GetBonePosition(6), 2, 3, Color(0, 255, 0), true)
+					--debugoverlay.Cross(tr.Entity:GetBonePosition(6) - Vector(math.random(-17, 17),math.random(-17, 17),math.random(0, 60)), 2, 3, Color(0, 0, 255), true)
+					
+			if self.Primary.ImpactSound then
+				self.Owner:EmitSound(self.Primary.ImpactSound)
+			end
+
+		-- if self.Primary.ImpactEffect then
+			-- local effect = EffectData()
+			-- effect:SetStart(tr.HitPos)
+			-- effect:SetNormal(tr.HitNormal)
+			-- effect:SetOrigin(tr.HitPos)
+
+			-- util.Effect(self.Primary.ImpactEffect, effect, true, true)
+		-- end
+
+		local ent = tr.Entity
+
+			if ent:IsPlayer() then
+				util.Decal("Blood", ent:GetBonePosition(6), ent:GetBonePosition(6) -Vector(math.random(-10, 10),math.random(-10, 10),math.random(0, 60)))
+				if self.Primary.FlashTime then
+					ent:ScreenFade(SCREENFADE.IN, self.Primary.FlashColour or color_white, self.Primary.FlashTime, 0)
+					ent.StunTime = CurTime() + self.Primary.FlashTime
+					ent.StunStartTime = CurTime()
+				elseif self.Primary.StunTime then
+					ent.StunTime = CurTime() + self.Primary.StunTime
+					ent.StunStartTime = CurTime()
+				end
+			end
+
+			if tr.MatType == MAT_FLESH then
+				ent:EmitSound("Flesh.ImpactHard")
+
+			elseif tr.MatType == MAT_WOOD then
+				ent:EmitSound("Wood.ImpactHard")
+			elseif tr.MatType == MAT_CONCRETE then
+				ent:EmitSound("Concrete.ImpactHard")
+			end
+		end
+			end
+		self.Owner:FireBullets(bullet)
+	--self:ClubAttack()
+	self:ViewPunch()
+		end
+	end
+end)
+		
+	else
+		if SERVER then
+		local bullet = {}
+		bullet.Num    = 1
+		bullet.AmmoType = "Snark"
+		bullet.Src    = self.Owner:GetShootPos()
+		bullet.Dir    = self.Owner:GetAimVector()
+		bullet.Spread = Vector(0, 0, 0)
+		bullet.Tracer = 0
+		bullet.Force  = 0
+		bullet.Hullsize = self.Primary.HullSize
+		bullet.Distance = self.Primary.Range + (self.Owner:GetVelocity():LengthSqr() / 950)
+		bullet.Damage = self.Primary.Damage
+		bullet.Callback = function(attacker, tr, dmginfo)
+					if tr.Hit then
+					
+			if self.Primary.ImpactSound then
+				self.Owner:EmitSound(self.Primary.ImpactSound)
+			end
+
+		if self.Primary.ImpactEffect then
+			local effect = EffectData()
+			effect:SetStart(tr.HitPos)
+			effect:SetNormal(tr.HitNormal)
+			effect:SetOrigin(tr.HitPos)
+
+			util.Effect(self.Primary.ImpactEffect, effect, true, true)
+		end
+
+		local ent = tr.Entity
+
+			if ent:IsPlayer() then
+				util.Decal("Blood", ent:GetBonePosition(6), ent:GetBonePosition(6) -Vector(math.random(-10, 10),math.random(-10, 10),math.random(0, 60)))
+				if self.Primary.FlashTime then
+					ent:ScreenFade(SCREENFADE.IN, self.Primary.FlashColour or color_white, self.Primary.FlashTime, 0)
+					ent.StunTime = CurTime() + self.Primary.FlashTime
+					ent.StunStartTime = CurTime()
+				elseif self.Primary.StunTime then
+					ent.StunTime = CurTime() + self.Primary.StunTime
+					ent.StunStartTime = CurTime()
+				end
+			end
+
+			if tr.MatType == MAT_FLESH then
+				ent:EmitSound("Flesh.ImpactHard")
+			elseif tr.MatType == MAT_WOOD then
+				ent:EmitSound("Wood.ImpactHard")
+			elseif tr.MatType == MAT_CONCRETE then
+				ent:EmitSound("Concrete.ImpactHard")
+			end
+		end
+			end	
+		self.Owner:FireBullets(bullet)
+			if self.Primary.DoStun then
+				self:StunAttack()
+			end
+
+			
+		--self:ClubAttack()
+		self:ViewPunch()
+	end
+	end
+
+	self:EmitSound(self.Primary.Sound)
+
+	self:SetNextPrimaryFire(CurTime() + self.Primary.Delay)
+
+	if self.DoFireAnim then
+		self:PlayAnim(ACT_VM_PRIMARYATTACK)
+	else
+		self:SendWeaponAnim(ACT_VM_HITCENTER)
+	end
+	
+	self.Owner:SetAnimation(PLAYER_ATTACK1)
+	
 end
 
 function SWEP:SecondaryAttack()
