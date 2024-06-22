3e9a5f1fbe9398b79f3f23afed0d3af0a9e50807
ForWhomTheBeatTolls <155327198+ForWhomTheBeatTolls@users.noreply.github.com>
2024-06-22 21:45:46 +0300

    Update sv_playermove.lua

--- gamemodes/skeleton-schema/schema/scripts/sv_playermove.lua
+++ gamemodes/skeleton-schema/schema/scripts/sv_playermove.lua
@@ -1,1 +1,22 @@
+local CMoveData = FindMetaTable("CMoveData")
 
+function CMoveData:RemoveKeys(keys)
+	-- Using bitwise operations to clear the key bits.
+	local newbuttons = bit.band(self:GetButtons(), bit.bnot(keys))
+	self:SetButtons(newbuttons)
+end
+
+hook.Add("SetupMove", "HatchetMovementRestrictFunctions", function(ply, mvd, cmd)
+	if mvd:KeyDown(IN_JUMP) then
+		if not ply.CanJump then
+			mvd:RemoveKeys(IN_JUMP)
+		end
+	end
+	
+	if mvd:KeyDown(IN_DUCK) then
+		if not ply.CanCrouch then
+			mvd:RemoveKeys(IN_DUCK)
+		end
+	end
+	
+end)
