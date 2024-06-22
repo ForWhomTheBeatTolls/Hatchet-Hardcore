7f4487a411faa44fe29f269169027684b4f3c77a
ForWhomTheBeatTolls <155327198+ForWhomTheBeatTolls@users.noreply.github.com>
2024-06-22 22:33:50 +0300

    Update cl_indoors.lua

--- gamemodes/skeleton-schema/plugins/indoors_ambience/cl_indoors.lua
+++ gamemodes/skeleton-schema/plugins/indoors_ambience/cl_indoors.lua
@@ -31,9 +31,9 @@ function DaMachine()
 	IsIndoors()
 
 	if indoor then
-		if impulse.GetSetting("ambience_sounds") then
-			day:PlayEx(0.17, 60)
-		end
+		-- if impulse.GetSetting("ambience_sounds") then
+			-- day:PlayEx(0.17, 60)
+		-- end
 		if not doneItOnce then
 			doneItOnce = true
 			if impulse.GetSetting("hud_ambience") then
@@ -46,9 +46,9 @@ function DaMachine()
 	end
 
 	if not indoor then
-		if impulse.GetSetting("ambience_sounds") then
-			day:Stop()
-		end
+		-- if impulse.GetSetting("ambience_sounds") then
+			-- day:Stop()
+		-- end
 		if doneItOnce then
 			doneItOnce = false
 			if impulse.GetSetting("hud_ambienceindoors") then
