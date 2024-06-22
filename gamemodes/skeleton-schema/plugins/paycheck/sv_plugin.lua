65ae43efb2a60c278b947261cf98ad44249115ab
ForWhomTheBeatTolls <155327198+ForWhomTheBeatTolls@users.noreply.github.com>
2024-06-22 18:09:31 +0300

    Paycheck adjustments

--- gamemodes/skeleton-schema/plugins/paycheck/sv_plugin.lua
+++ gamemodes/skeleton-schema/plugins/paycheck/sv_plugin.lua
@@ -1,17 +1,18 @@
-local paycheckdelay = CurTime() + 120
+local paycheckdelay = CurTime() + 600
 
 function PLUGIN:Think()
-		if CurTime() > paycheckdelay then
-			for k,v in pairs(player.GetAll()) do
-				if v:Team() == TEAM_CITIZEN then
+	if CurTime() > paycheckdelay then
+		for k,v in pairs(player.GetAll()) do
+			if v:Team() == TEAM_CITIZEN then
+				v:GiveBankMoney(5)
+			elseif v:Team() == TEAM_CP then
+				v:GiveBankMoney(15)
+			elseif v:Team() == TEAM_WORKFORCE
 				v:GiveBankMoney(10)
-				elseif v:Team() == TEAM_CP then
-				v:GiveBankMoney(30)
-				elseif v:Team() == TEAM_RESISTANCE then
+			elseif v:Team() == TEAM_RESISTANCE then
 				v:GiveBankMoney(1)
 			end
 		end
-		paycheckdelay = CurTime() + 120
+	paycheckdelay = CurTime() + 600
 	end
 end
-			
