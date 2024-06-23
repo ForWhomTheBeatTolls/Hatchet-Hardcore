f7900ff74db20581d6a782382d4a233cc83e11e9
WillMaster <79755822+willmasterLUA@users.noreply.github.com>
2024-06-23 19:09:40 +0200

    DISPATCH FACTION !  !  !!  ! ! !!  ! !! !

---
+++ gamemodes/skeleton-schema/schema/teams/sh_e_dispatch.lua
@@ -0,0 +1,19 @@
+TEAM_DISPATCH = impulse.Teams.Define({
+	name = "Dispatch",
+	color = Color(0, 76, 255),
+	description = [[Dispatch.]],
+	loadout = {"impulse_hands"},
+	salary = 250,
+	model = "models/Police.mdl",
+	handModel = "",
+	percentLimit = true,
+	limit = 1,
+	xp = 0,
+	cp = true,
+	doorGroup = {1},
+	blockNameChange = true,
+	runSpeed = 210,
+
+    nameFormat = "DISPATCH",
+})
+
