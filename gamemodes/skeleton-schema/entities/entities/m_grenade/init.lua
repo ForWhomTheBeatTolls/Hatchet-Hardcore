4e91311c1e7578989a3aa5f971336a1b53ffd121
ForWhomTheBeatTolls <155327198+ForWhomTheBeatTolls@users.noreply.github.com>
2024-06-28 02:01:06 +0300

    Improve grenades

--- gamemodes/skeleton-schema/entities/entities/m_grenade/init.lua
+++ gamemodes/skeleton-schema/entities/entities/m_grenade/init.lua
@@ -9,6 +9,7 @@ function ENT:Initialize()
 	self:PhysicsInit(SOLID_VPHYSICS)
 	self:SetMoveType(MOVETYPE_VPHYSICS)
 	self:SetCollisionGroup(COLLISION_GROUP_WEAPON)
+	self:SetHealth(40)
 	self.TickCount = 0
 	self.Increment = 1.5
 	self.NextTick = CurTime() + (self.Increment - 0.4)
@@ -32,15 +33,23 @@ function ENT:Think()
 	if self.Increment <= 0 then
 		self:Boom()
 	end
+	if (self:Health() <= 0) then
+		self:Boom()
+	end
+end
+
+function ENT:OnTakeDamage( dmginfo )
+	local damage = dmginfo:GetDamage()
+		self:SetHealth(self:Health() - damage)
 end
 
 function ENT:Boom()
-	local push_force = 1500
+	local push_force = 2500
 	local explode = ents.Create("env_explosion")
 	explode:SetPos(self:GetPos())
 	explode:Spawn()
 	explode:SetKeyValue("iMagnitude", "0")
-	explode:SetKeyValue("DamageForce", "180")
+	explode:SetKeyValue("DamageForce", "380")
 	explode:Fire( "Explode", 0, 0 )
 	for k,v in pairs(ents.FindInSphere(explode:GetPos(), 404)) do
 		
@@ -70,18 +79,55 @@ function ENT:Boom()
 		
 		end
 		
-		if IsValid(v:GetPhysicsObject()) then
+		if v:IsLineOfSightClear(self) and v:GetClass() == "impulse_item" then
+			
+			diff = center - v:GetPos()
+			d = diff:Dot(diff)
+			
+			d = math.max(0 , math.sqrt(d) - 190)
+			dmg = -0.01 * (d^2) + 125
+			
+			local dmginfo = DamageInfo()
+			dmginfo:SetDamage(dmg)
+			dmginfo:SetDamageType(DMG_BLAST)
+			dmginfo:SetInflictor(self)
+			dmginfo:SetDamagePosition(self:GetPos())
+			
+			v:TakeDamageInfo(dmginfo)
+			
+		
+		end
+		
+		if v:GetPhysicsObject():IsValid() then
 			
 			local vpos = v:LocalToWorld(v:OBBCenter())
 			local spos = self:GetPos()
 			local dir = (vpos - spos):GetNormal()
 			local phys = v:GetPhysicsObject()
 			
-			dir.z = math.abs(dir.z) + 2
+			dir.z = math.abs(dir.z) + (1 * (phys:GetMass() / 25))
+			
+			diff = center - v:GetPos()
+			d = diff:Dot(diff)
+			
+			d = math.max(0 , math.sqrt(d) - 190)
+			dmg = -0.01 * (d^2) + 125
+			
+			local dmginfo = DamageInfo()
+			dmginfo:SetDamage(dmg)
+			dmginfo:SetDamageType(DMG_BLAST)
+			dmginfo:SetInflictor(self)
+			dmginfo:SetDamagePosition(self:GetPos())
+			
+			v:TakeDamageInfo(dmginfo)
+			
 			
 			phys:ApplyForceCenter(dir * 1 * push_force)
 			
 		end
+		
+		
+		
 	end
 	self:Remove()
 end
