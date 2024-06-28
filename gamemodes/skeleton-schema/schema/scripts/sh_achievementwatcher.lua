6291def4700047a06e968398df7fe2d126223652
ForWhomTheBeatTolls <155327198+ForWhomTheBeatTolls@users.noreply.github.com>
2024-06-28 17:21:31 +0300

    Hopefully fix achievements

--- gamemodes/skeleton-schema/schema/scripts/sh_achievementwatcher.lua
+++ gamemodes/skeleton-schema/schema/scripts/sh_achievementwatcher.lua
@@ -1,20 +1,24 @@
 if SERVER then
-	hook.Add("PlayerDeath", "impulseAchDeath", function(ply, inflictor, attacker)
-	if SERVER then
-		if not attacker:IsPlayer() then return end
-		if not ply:Alive() then
-			ply:AchievementGive("ach_adie")
-		end
-		if attacker:IsPlayer() and attacker:Alive() then
-			attacker:AchievementGive("ach_akill")
+	
+hook.Add("PlayerDeath", "impulseAchDeath", function(ply, inflictor, attacker)
+		if attacker:IsPlayer() then
+			if not attacker:AchievementHas("ach_akill") == true then
+				attacker:AchievementGive("ach_akill")
+			end
 		end
 		if ply:Team() == TEAM_OTA and (attacker:IsPlayer()) and (attacker != ply) then
-			attacker:AchievementGive("ach_combine2")
+			if not attacker:AchievementHas("ach_acombine2") == true then
+				attacker:AchievementGive("ach_combine2")
+			end
 		end
 		if ply:Team() == TEAM_CP and (attacker:IsPlayer()) and (attacker != ply) then
-			attacker:AchievementGive("ach_combine1")
+			if not attacker:AchievementHas("ach_acombine1") == true then
+				attacker:AchievementGive("ach_combine1")
+			end
 		end
-	end
+		
+		ply:AchievementTake("ach_adie")
+		ply:AchievementGive("ach_adie")
 end)
 	hook.Add("PlayerSay", "impulseChatAchWatch", function(ply, text)
 			if
@@ -22,7 +26,10 @@ end)
 		text == "I keep falling, but never falling six feet deep" or
 		text == "i keep falling, but never falling six feet deep." or
 		text == "i keep falling, but never falling six feet deep" then
-		ply:AchievementGive("ach_6feet")
+				if not ply:AchievementHas("ach_a6feet") then
+					ply:AchievementGive("ach_6feet")
+				end
 			end
 		end)
-    end
+		
+end
