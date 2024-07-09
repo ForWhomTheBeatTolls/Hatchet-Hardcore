a458baf91124ab21852e2a34922f2644e9c53f4b
WillMaster <79755822+willmasterLUA@users.noreply.github.com>
2024-07-09 18:31:50 +0200

    Update sh_apply.lua

--- gamemodes/skeleton-schema/schema/scripts/sh_apply.lua
+++ gamemodes/skeleton-schema/schema/scripts/sh_apply.lua
@@ -12,14 +12,38 @@ local CitizenApplyCommand = {
 		if ply:Team() == TEAM_CITIZEN then team = "Citizen"
 		elseif ply:Team() == TEAM_WORKFORCE then team = "Workforce"
 		elseif ply:Team() == TEAM_CP then team = "Civil Protection"
-		end		
-		
+		end
+
 		if table.HasValue(CanApplyTeams, ply:Team()) then
+
 			ply:SetNWBool("Applied", true)
 			ply:SetNWInt("ApplyWearTime", CurTime() + 300 )
+
 			for v,k in pairs(player.GetAll()) do
-				if (ply:GetPos() - k:GetPos()):LengthSqr() <= (impulse.Config.TalkDistance ^ 2) then 
-					k:SendChatClassMessage(20, (ply:Nick().." | "..team), ply)
+				if (ply:GetPos() - k:GetPos()):LengthSqr() <= (impulse.Config.TalkDistance ^ 2) then
+					k:SendChatClassMessage(20, ply:Nick() .. " | " .. team, ply)
+					local tabl = util.JSONToTable(k:GetSyncVar(SYNC_RECOGNIZES))
+
+					if k:SteamID() == ply:SteamID() then continue end
+
+					if k:IsCP() then return end
+
+					if tabl[ply:SteamID()] then
+						return
+					else
+
+						AddIntroduction(ply, k)
+
+						if tabl[k:SteamID()] then
+							ply:Notify( "You introduced yourself to " .. k:Name() .. " while you were applying." )
+							k:Notify(ply:Name() .. " Introduced himself to you while they were applying.")
+						else
+							ply:Notify( "You introduced yourself to an unknown person while you were applying." )
+							k:Notify(ply:Name() .. " Introduced himself to you while they were applying.")
+						end
+					end
+
+
 				end
 			end
 		else
