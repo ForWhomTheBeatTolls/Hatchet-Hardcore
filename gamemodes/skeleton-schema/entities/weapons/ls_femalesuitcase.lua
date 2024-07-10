8ef153667142fa075709ea16b24b8f0049029ad5
WillMaster <79755822+willmasterLUA@users.noreply.github.com>
2024-07-10 18:49:51 +0200

    Add files via upload

---
+++ gamemodes/skeleton-schema/entities/weapons/ls_femalesuitcase.lua
@@ -0,0 +1,27 @@
+AddCSLuaFile()
+
+SWEP.Base = "ls_base_melee"
+
+SWEP.PrintName = "Suitcase"
+SWEP.Category = "Hatchet"
+
+SWEP.Spawnable = true
+SWEP.AdminOnly = false
+
+SWEP.HoldType = "suitcase"
+
+SWEP.WorldModel = Model("models/weapons/w_hatchetsuitcase.mdl")
+
+SWEP.ViewModel = Model("models/weapons/c_crowbar.mdl")
+SWEP.ViewModelFOV = 65
+
+SWEP.Slot = 2
+SWEP.SlotPos = 1
+
+SWEP.IsAlwaysRaised = true
+
+function SWEP:PrimaryAttack()
+end
+
+function SWEP:SecondaryAttack()
+end
