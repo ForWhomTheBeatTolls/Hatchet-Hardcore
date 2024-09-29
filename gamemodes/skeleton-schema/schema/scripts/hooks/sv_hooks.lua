f948fe50fce01098ece0715a12ad70c1fd3573b5
WillMaster <79755822+willmasterLUA@users.noreply.github.com>
2024-09-29 16:17:05 +0200

    Fix and Update sv_hooks.lua

--- gamemodes/skeleton-schema/schema/scripts/hooks/sv_hooks.lua
+++ gamemodes/skeleton-schema/schema/scripts/hooks/sv_hooks.lua
@@ -6,25 +6,30 @@
 		ply.TimesStunnedCool = CurTime()
 		ply:SetNWInt("CombatCool", CurTime())
 		ply:SetNWInt("ApplyWearTime", CurTime() + 120)
-		ply.IsInASequence = false
 		ply:SetNWBool("IsInCombat", false)
 		ply:SetNWBool("Applied", true)
+		ply:SetNWInt("CarryWeight", 25)
+		ply.IsInASequence = false
+		ply.FoodPoisoning = false
+		ply.Stunned = false
+		ply.KillMoved = false
+		ply.HealOverTime = false
 		ply.CanCrouch = true
 		ply.CanJump = true
-		ply.Stunned = false
+		ply.Bleeding = false
+		ply:SetNWInt("BleedRate", 0)
+		ply.NextBleed = CurTime()
 		ply.TimesDamaged = 0
 		ply.CombatCool = 0
 		ply.TimesStunned = 0
-		ply:SetNWInt("CarryWeight", 25)
-		ply.FoodPoisoning = false
+		ply.CrouchCount = 0
 		ply.PendingReward = ply.PendingReward or 0
-		ply.HealOverTime = false
 		ply:AddEFlags(EFL_NO_DAMAGE_FORCES)
 		ply:RemoveAllDecals()
 	end
 
 	hook.Add("PostEntityTakeDamage","hatchetdamagefunctions",function(ent, dmg, took)
-		if ent:IsPlayer() and took and ent.NextHurtSound < CurTime() then
+		if ent:IsPlayer() and took and ent.NextHurtSound < CurTime() and dmg:GetDamage() > 10 then
 		
 			if dmg:IsDamageType(DMG_NERVEGAS) then
 				ent:EmitSound("vo/npc/male01/moan0"..math.random(1,5)..".wav", 50)
@@ -67,8 +72,6 @@
 				elseif ply:KeyDown(IN_SPEED) then
 					ply:EmitSound("npc/metropolice/gear"..math.random(1,6)..".wav", 70)
 				end
-
-				return true
 			end
 			
 			if ply:Team() == TEAM_OTA  then
@@ -77,8 +80,6 @@
 				elseif ply:KeyDown(IN_SPEED) then
 					ply:EmitSound("npc/combine_soldier/gear"..math.random(1,6)..".wav", 75)
 				end
-
-				return true
 			end
 
 			if ply:Team() == TEAM_VORTIGAUNT  then
@@ -100,6 +101,9 @@
 			if ply:Team() == TEAM_CITIZEN or ply:Team() == TEAM_WORKFORCE and not ply.HasVest then
 				return false
 			end
+			
+		return true
+			
 	end)
 
 	local deathsounds = {
@@ -409,6 +413,9 @@ local function callhookremoval()
 end
 
 hook.Add("PlayerDeath", "ZombificationTransformer", function(victim, inflictor, attacker)
+	if not IsValid(attacker) then return end
+	if not IsValid(victim) then return end
+	
 	if attacker:GetClass() == "npc_headcrab" then
 		local headcrab = ents.Create("npc_zombie")
 		headcrab:SetPos(victim:GetPos())
