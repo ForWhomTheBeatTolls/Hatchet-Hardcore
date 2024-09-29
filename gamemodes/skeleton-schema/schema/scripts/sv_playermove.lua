98672e05360382ec6d14ad1b2eac525b963800ab
WillMaster <79755822+willmasterLUA@users.noreply.github.com>
2024-09-29 16:23:12 +0200

    undo

--- gamemodes/skeleton-schema/schema/scripts/sv_playermove.lua
+++ gamemodes/skeleton-schema/schema/scripts/sv_playermove.lua
@@ -1,4 +1,5 @@
 local CMoveData = FindMetaTable("CMoveData")
+
 function CMoveData:RemoveKeys(keys)
 	-- Using bitwise operations to clear the key bits.
 	local newbuttons = bit.band(self:GetButtons(), bit.bnot(keys))
@@ -6,27 +7,93 @@ function CMoveData:RemoveKeys(keys)
 end
 
 hook.Add("SetupMove", "HatchetMovementRestrictFunctions", function(ply, mvd, cmd)
+
 	if mvd:KeyDown(IN_JUMP) then
-		if not ply.CanJump then mvd:RemoveKeys(IN_JUMP) end
-		if ply:Team() == TEAM_DISPATCH then mvd:RemoveKeys(IN_JUMP) end
+	
+		if not ply.CanJump then
+			mvd:RemoveKeys(IN_JUMP)
+		end
+		
+		if ply:Team() == TEAM_DISPATCH then
+			mvd:RemoveKeys(IN_JUMP)
+		end
+		
 	end
-
+	
 	if mvd:KeyDown(IN_DUCK) then
-		if not ply.CanCrouch then mvd:RemoveKeys(IN_DUCK) end
-		if ply:Team() == TEAM_DISPATCH then mvd:RemoveKeys(IN_DUCK) end
+	
+		if not ply.CanCrouch then
+			mvd:RemoveKeys(IN_DUCK)
+		end
+		
+		if ply.CrouchCount > 5 then
+			mvd:RemoveKeys(IN_DUCK)
+		end
+		
+		if ply:Team() == TEAM_DISPATCH then
+			mvd:RemoveKeys(IN_DUCK)
+		end
+		
 	end
 
-	if mvd:KeyDown(IN_FORWARD) or mvd:KeyDown(IN_MOVELEFT) then if ply:Team() == TEAM_DISPATCH then mvd:SetForwardSpeed(0) end end
+	if mvd:KeyDown(IN_FORWARD) then
+	
+		if ply:Team() == TEAM_DISPATCH then
+			mvd:SetForwardSpeed( 0 )
+		end
+		
+	end
+
+	if mvd:KeyDown(IN_MOVELEFT) then
+	
+		if ply:Team() == TEAM_DISPATCH then
+			mvd:SetSideSpeed( 0 )
+		end
+		
+	end
+
 	if mvd:KeyDown(IN_SPEED) then
-		if ply:GetActiveWeapon():GetClass() == "ls_suitcase" or ply:GetActiveWeapon():GetClass() == "ls_femalesuitcase" and ply:Team() != TEAM_VORTIGAUNT then
-			if ply:IsWalking() then
-				mvd:SetForwardSpeed(impulse.Config.WalkSpeed)
-			else
-				mvd:SetForwardSpeed(0)
+	
+		if ply:Alive() then
+			if ply:GetActiveWeapon():GetClass() == "ls_suitcase" or ply:GetActiveWeapon():GetClass() == "ls_femalesuitcase" then
+				if ply:IsWalking() then
+					mvd:SetForwardSpeed( impulse.Config.WalkSpeed )
+				else
+					mvd:SetForwardSpeed( 0 )
+				end
 			end
 		end
+		
 	end
 
-	if mvd:KeyDown(IN_MOVERIGHT) then if ply:Team() == TEAM_DISPATCH then mvd:SetSideSpeed(0) end end
-	if mvd:KeyDown(IN_BACK) then if ply:Team() == TEAM_DISPATCH then mvd:SetForwardSpeed(0) end end
+
+
+	if mvd:KeyDown(IN_MOVERIGHT) then
+	
+		if ply:Team() == TEAM_DISPATCH then
+			mvd:SetSideSpeed( 0 )
+		end
+		
+	end
+
+	if mvd:KeyDown(IN_BACK) then
+	
+		if ply:Team() == TEAM_DISPATCH then
+			mvd:SetForwardSpeed( 0 )
+		end
+		
+	end
+	
 end)
+
+hook.Add( "KeyPress", "CrouchCountThenTwist", function( ply, key )
+	if ( key == IN_DUCK ) then
+		print( ply.CrouchCount )
+		ply.CrouchCount = ply.CrouchCount + 1
+		if ply.CrouchCount > 5 then
+			ply.CanCrouch = false
+			ply:Notify("You are fatigued. Wait before attempting to crouch again.")
+			--timer.Simple(3, function() if IsValid(ply) then ply.CanCrouch = true ply.CrouchCount = 0 end end)
+		end
+	end
+end )
