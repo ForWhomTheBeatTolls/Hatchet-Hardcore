b791499cdfb1bb7d1bb79bdc6655e315e6099d6d
ForWhomTheBeatTolls <155327198+ForWhomTheBeatTolls@users.noreply.github.com>
2024-09-29 14:26:25 +0300

    Update sh_util_fuel.lua

--- gamemodes/skeleton-schema/schema/items/sh_util_fuel.lua
+++ gamemodes/skeleton-schema/schema/items/sh_util_fuel.lua
@@ -1,20 +1,61 @@
 local ITEM = {}
 
 ITEM.UniqueID = "util_fuel"
-ITEM.Name = "Fuel Can"
-ITEM.Desc = "A can full of fuel."
-ITEM.Model = Model("models/props_junk/metalgascan.mdl")
-ITEM.FOV = 20.651862464183
-ITEM.CamPos = Vector(54.441261291504, 76.790832519531, 23.49570274353)
-ITEM.Weight = 9
+ITEM.Name = "Gasoline Canister"
+ITEM.Desc = "A jerry can filled with flammable fuel."
+ITEM.Weight = 10
+ITEM.Model = Model("models/props_junk/gascan001a.mdl")
+ITEM.FOV = 16.568767908309
+ITEM.CamPos = Vector(-17.191976547241, 16.618911743164, 100)
 
 ITEM.Droppable = true
-ITEM.DropOnDeath = false
+ITEM.DropOnDeath = true
 
-ITEM.Illegal = false
-ITEM.CanStack = true
+ITEM.Illegal = true
+ITEM.CanStack = false
 
-ITEM.CraftSound = "fuel"
-ITEM.CraftTime = 4
+ITEM.UseName = "Fill"
+ITEM.UseWorkBarTime = 2
+ITEM.UseWorkBarName = "Filling..."
+ITEM.UseWorkBarSound = "ambient/water/leak_1.wav"
+ITEM.UseWorkBarFreeze = true
+ITEM.UseWorkBarSoundLooping = true 
+
+function ITEM:OnUse(ply, door)
+
+		ply:DoCustomAnimEvent( PLAYERANIMEVENT_ATTACK_GRENADE, 279 )
+			if door:GetClass() == "m_barrel" and door:GetNWInt("Filled") == false then
+				door:EmitSound("ambient/water/water_spray1.wav")
+				door:SetNWInt("Filled", true)
+				ply:Say("/me fills up a barrel with fuel.")	
+				return true
+			else 
+				ply:Notify("You need something to pour this into.")
+			end
+		
+
+
+	    -- --ply:EmitSound("weapons/357/357_reload4.wav")
+		-- --ply:Notify("You have successfully lockpicked the door.")
+	
+	end
+
+function ITEM:ShouldTraceUse(ply, ent)
+    -- if ent:IsPropDoor() then
+        -- return false
+    -- end
+	
+	-- if not ent:IsDoor() then
+		-- return false
+	-- end
+
+	local group = ent:GetSyncVar(SYNC_DOOR_GROUP, nil)
+
+	if ent:GetNWInt("Filled", true) == false then
+		return true
+	else
+		return false
+	end
+end
 
 impulse.RegisterItem(ITEM)
