ab749f4bc730d377367bfa9ae752b14bffc79b7f
WillMaster <79755822+willmasterLUA@users.noreply.github.com>
2024-09-30 15:33:38 +0200

    Fix muhammed poor decisions

--- gamemodes/skeleton-schema/plugins/example_plugin/sv_test.lua
+++ gamemodes/skeleton-schema/plugins/example_plugin/sv_test.lua
@@ -1,9 +1,3 @@
-function PLUGIN:PlayerSay(ply, text)
+//function PLUGIN:PlayerSay(ply, text)
 	//ply:Notify("You just said: "..text)
-end
-
-function PLUGIN:PlayerDeath(victim, wep, attacker)
-	if IsValid(attacker) and attacker:IsPlayer() then
-		attacker:AchievementGive("ach_kill")
-	end
-end
+//end
