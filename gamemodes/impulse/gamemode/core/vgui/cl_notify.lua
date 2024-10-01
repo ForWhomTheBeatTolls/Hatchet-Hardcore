6b10edc494ac7479f56039fe854df4ac3544e6ea
WillMaster <79755822+willmasterLUA@users.noreply.github.com>
2024-10-01 23:22:08 +0200

    Update cl_notify.lua

--- gamemodes/impulse/gamemode/core/vgui/cl_notify.lua
+++ gamemodes/impulse/gamemode/core/vgui/cl_notify.lua
@@ -1,13 +1,21 @@
 local PANEL = {}
 
-local baseSizeW, baseSizeH = ScrW(), 40
+local baseSizeW, baseSizeH = 300, 20
 
 function PANEL:Init()
 	self.message = markup.Parse("")
 	self:SetSize(baseSizeW, baseSizeH)
 	self.startTime = CurTime()
 	self.endTime = CurTime() + 7.5
-	self:SetAlpha(0)
+	self.bordercol = impulse.Config.MainColour
+	self.notifsound = "hatchet/hint.wav"
+end
+
+function PANEL:SetPanelSettings(col, snd)
+	self.bordercol = col
+	self.notifsound = snd
+
+	surface.PlaySound(self.notifsound)
 end
 
 function PANEL:SetMessage(...)
@@ -27,36 +35,43 @@ function PANEL:SetMessage(...)
 	msg = msg.."</font>"
 
 	-- parse
-	self.message = markup.Parse(msg, baseSizeW - 140)
+	self.message = markup.Parse(msg, baseSizeW-20)
 
 	-- set frame position and height to suit the markup
 	local shiftHeight = self.message:GetHeight()
 	self:SetHeight(shiftHeight+baseSizeH)
-	surface.PlaySound("hatchet/hint.wav")
+	surface.PlaySound(self.notifsound)
+
 end
 
-local gradient = Material("vgui/gradient_up")
-local darkCol = Color(0, 0, 0)
-local lightCol = Color(20,20,20,80)
+local gradient = Material("gui/gradient_up")
+local gradient2 = Material("vgui/gradient-r")
+local darkCol = Color(0, 0, 0, 227)
+local lightCol = Color(0,0,0,208)
 local hudBlackGrad = Color(40,40,40,120)
 local lifetime = 10
 
 function PANEL:Paint(w,h)
 	-- draw frame
-	//impulse.blur(self, 10, 20, 255)
-	//surface.SetDrawColor(darkCol)
-	//surface.DrawRect(0,0,w,h)
-	//surface.SetDrawColor(darkCol)
-	//surface.SetMaterial(gradient)
-	//surface.DrawTexturedRect(0,0,w,h)
+	impulse.blur(self, 10, 20, 255)
+	surface.SetDrawColor(darkCol)
+	surface.DrawRect(0,0,w,h)
+	surface.SetDrawColor(darkCol)
+	surface.SetMaterial(gradient)
+	surface.DrawTexturedRect(0,0,w,h)
 
 	-- draw message
-	self.message:Draw(ScrW() * .5, 20, TEXT_ALIGN_CENTER, TEXT_ALIGN_BOTTOM)
+	self.message:Draw(10,10, TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
 
 	-- draw timebar
-	//local w2 = math.TimeFraction(self.startTime, self.endTime, CurTime()) * w
-	//surface.SetDrawColor(Color(impulse.Config.MainColour.r, impulse.Config.MainColour.g, impulse.Config.MainColour.b))
-	//surface.DrawRect(w2, h-2, w - w2, 4)
+	local w2 = math.TimeFraction(self.startTime, self.endTime, CurTime()) * w
+	surface.SetDrawColor(self.bordercol)
+	surface.DrawRect(w2, h-2, w - w2, 2)
+	//surface.SetDrawColor(self.bordercol)
+	surface.DrawRect(285, 0, 20, h)
+	surface.SetDrawColor(lightCol)
+	surface.SetMaterial(gradient2)
+	surface.DrawTexturedRect(265, 0, 40, h)
 end
 
 vgui.Register("impulseNotify", PANEL, "DPanel")
