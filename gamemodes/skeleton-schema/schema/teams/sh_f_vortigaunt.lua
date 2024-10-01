eb135e9004ebfb89de2360fc65baea6b11622c3f
WillMaster <79755822+willmasterLUA@users.noreply.github.com>
2024-10-01 14:17:45 +0200

    Update sh_f_vortigaunt.lua

--- gamemodes/skeleton-schema/schema/teams/sh_f_vortigaunt.lua
+++ gamemodes/skeleton-schema/schema/teams/sh_f_vortigaunt.lua
@@ -1,12 +1,19 @@
 TEAM_VORTIGAUNT = impulse.Teams.Define({
     name = "Vortigaunt",
     color = Color(120, 139, 65),
-    loadout = {"impulse_hands", "weapon_physgun", "gmod_tool"},
+    loadout = {"impulse_hands", "weapon_physgun", "gmod_tool", "ls_broom"},
     description = [[Clean the streets.]],
     salary = 10,
     model = "models/vortigaunt_slave.mdl",
     handModel = "models/weapons/c_arms_hev.mdl",
     percentLimit = false,
     nameFormat = "Vort",
-    OnBecome = function(ply) print("Become") end
+    onBecome = function(ply)
+        if ply:GetSyncVar(SYNC_RPVORTNAME) == "" then
+            net.Start("HatchetVortRPName")
+            net.Send(ply)
+        else
+            ply:SetRPName(ply:GetSyncVar(SYNC_RPVORTNAME))
+        end
+    end
 })
