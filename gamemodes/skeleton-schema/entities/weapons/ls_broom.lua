04fcfaab394114d4cb8423e0e02fb55a108d98ba
WillMaster <79755822+willmasterLUA@users.noreply.github.com>
2024-10-01 14:18:15 +0200

    BROOM

---
+++ gamemodes/skeleton-schema/entities/weapons/ls_broom.lua
@@ -0,0 +1,85 @@
+AddCSLuaFile()
+
+SWEP.Base = "ls_base_melee"
+
+SWEP.PrintName = "Broom"
+SWEP.Category = "Hatchet"
+
+SWEP.Spawnable = true
+SWEP.AdminOnly = false
+
+SWEP.HoldType = "ar2"
+
+SWEP.WorldModel = Model("")
+SWEP.ViewModel = Model("models/weapons/v_crowbar.mdl")
+SWEP.ViewModelFOV = 65
+
+SWEP.Slot = 1
+SWEP.SlotPos = 2
+
+SWEP.CSMuzzleFlashes = false
+
+SWEP.IsAlwaysRaised = true
+
+SWEP.Primary.Sound = Sound("WeaponFrag.Roll")
+
+SWEP.Primary.ImpactSound = Sound("physics/body/body_medium_impact_hard" .. math.random(1, 6) .. ".wav")
+
+
+SWEP.Primary.ImpactSoundWorldOnly = true
+SWEP.Primary.Recoil = 2 -- base recoil value, SWEP.Spread mods can change this
+SWEP.Primary.Damage = 5 -- not used in this swep
+SWEP.Primary.NumShots = 1
+SWEP.Primary.HitDelay = 0.3
+SWEP.Primary.HullSize = 4
+SWEP.Primary.Delay = 0.7
+SWEP.Primary.Range = 50
+SWEP.Primary.StunTime = 0.1
+SWEP.Primary.Automatic = true
+
+SWEP.BlockDelay = CurTime()
+
+function SWEP:PrimaryAttack()
+	if not self:GetOwner():Team() == TEAM_VORT then
+		return true
+	end
+
+	if not self:GetOwner():OnGround() then
+		return
+	end
+
+	if SERVER then
+		self:GetOwner():ForceSequence("sweep")
+		self:GetOwner():EmitSound("impulse/broom.wav")
+	end
+
+	self:SetNextPrimaryFire(CurTime() + 3)
+end
+
+if SERVER then
+
+	local broom = ents.Create("prop_dynamic")
+
+	function SWEP:Deploy()
+		if not IsValid(broom) then
+			broom = ents.Create("prop_dynamic")
+		end
+		broom:SetModel("models/props_c17/pushbroom.mdl")
+		broom:DrawShadow(true)
+		broom:SetMoveType(MOVETYPE_NONE)
+		broom:SetParent(self:GetOwner())
+		broom:SetSolid(SOLID_NONE)
+		broom:Spawn()
+		broom:SetName(self:GetOwner():SteamID64() .. "_Broom")
+		broom:Fire("setparentattachment", "cleaver_attachment", 0.01)
+	end
+
+	function SWEP:Holster()
+		-- print("Broom: " .. broom:GetName())
+		if broom:GetName() == self:GetOwner():SteamID64() .. "_Broom" then
+			broom:Remove()
+			return true
+		end
+	end
+
+end
