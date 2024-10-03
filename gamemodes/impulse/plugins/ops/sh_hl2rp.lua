117a488b180c8686023abb0beab4530e2bd1a62d
WillMaster <79755822+willmasterLUA@users.noreply.github.com>
2024-10-03 21:50:08 +0200

    Whitelists

--- gamemodes/impulse/plugins/ops/sh_hl2rp.lua
+++ gamemodes/impulse/plugins/ops/sh_hl2rp.lua
@@ -3,7 +3,7 @@ if SERVER then
 end
 
 local combineBanCommand = {
-    description = "Gives the player a combine ban for the time specified (max of 1 week).",
+    description = "Gives the player a combine ban for the time specified (In seconds)",
     requiresArg = true,
     adminOnly = true,
     onRun = function(ply, arg, rawText)
@@ -16,8 +16,6 @@ local combineBanCommand = {
 		end
 
 		time = tonumber(time)
-		time = time * 60
-		time = math.Clamp(time, 0, 604800)
 
 		if plyTarget and plyTarget.impulseData then
 			local curT = os.time()
@@ -66,3 +64,212 @@ local combineUnBanCommand = {
 }
 
 impulse.RegisterChatCommand("/uncombineban", combineUnBanCommand)
+
+
+
+
+
+
+
+
+
+local GiveOSWhitelist = {
+    description = "Gives a player the whitelist to the OS Faction.",
+    requiresArg = true,
+    adminOnly = true,
+    onRun = function(ply, arg, rawText)
+        local name = arg[1]
+		local plyTarget = impulse.FindPlayer(name)
+
+		if plyTarget and plyTarget.impulseData then
+
+			plyTarget.impulseData.OSWhitelist = true
+			plyTarget:SaveData()
+
+			ply:Notify("You have given OS whitelist to "..plyTarget:Nick()..".")
+			plyTarget:Notify("You have been given OS Whitelist by a game moderator ("..ply:SteamName()..").")
+		else
+			return ply:Notify("Could not find player: "..tostring(name))
+		end
+    end
+}
+
+impulse.RegisterChatCommand("/giveoswhitelist", GiveOSWhitelist)
+
+local RemoveOSWhitelist = {
+    description = "Remove a player's OS whitelist.",
+    requiresArg = true,
+    adminOnly = true,
+    onRun = function(ply, arg, rawText)
+        local name = arg[1]
+		local plyTarget = impulse.FindPlayer(name)
+
+		if plyTarget and plyTarget.impulseData then
+			plyTarget.impulseData.OSWhitelist = nil
+			plyTarget:SaveData()
+
+			plyTarget:Notify("Your OS Whitelist has been removed by a game moderator ("..ply:SteamName()..").")
+			ply:Notify("You have removed the OS Whitelist from "..plyTarget:Nick()..".")
+		else
+			return ply:Notify("Could not find player: "..tostring(name))
+		end
+    end
+}
+
+impulse.RegisterChatCommand("/removeoswhitelist", RemoveOSWhitelist)
+
+
+
+
+
+
+local GiveEchoWhitelist = {
+    description = "Gives a player the whitelist to the ECHO Rank.",
+    requiresArg = true,
+    adminOnly = true,
+    onRun = function(ply, arg, rawText)
+        local name = arg[1]
+		local plyTarget = impulse.FindPlayer(name)
+
+		if plyTarget and plyTarget.impulseData then
+
+			plyTarget.impulseData.EchoWhitelist = true
+			plyTarget:SaveData()
+
+			ply:Notify("You have given ECHO whitelist to "..plyTarget:Nick()..".")
+			plyTarget:Notify("You have been given ECHO Whitelist by a game moderator ("..ply:SteamName()..").")
+		else
+			return ply:Notify("Could not find player: "..tostring(name))
+		end
+    end
+}
+
+impulse.RegisterChatCommand("/giveechowhitelist", GiveEchoWhitelist)
+
+local RemoveEchoWhitelist = {
+    description = "Remove a player's Echo whitelist.",
+    requiresArg = true,
+    adminOnly = true,
+    onRun = function(ply, arg, rawText)
+        local name = arg[1]
+		local plyTarget = impulse.FindPlayer(name)
+
+		if plyTarget and plyTarget.impulseData then
+			plyTarget.impulseData.EchoWhitelist = nil
+			plyTarget:SaveData()
+
+			plyTarget:Notify("Your ECHO Whitelist has been removed by a game moderator ("..ply:SteamName()..").")
+			ply:Notify("You have removed the ECHO Whitelist from "..plyTarget:Nick()..".")
+		else
+			return ply:Notify("Could not find player: "..tostring(name))
+		end
+    end
+}
+
+impulse.RegisterChatCommand("/removeechowhitelist", RemoveEchoWhitelist)
+
+
+
+
+
+
+
+
+local GiveMaceWhitelist = {
+    description = "Gives a player the whitelist to the Mace Rank.",
+    requiresArg = true,
+    adminOnly = true,
+    onRun = function(ply, arg, rawText)
+        local name = arg[1]
+		local plyTarget = impulse.FindPlayer(name)
+
+		if plyTarget and plyTarget.impulseData then
+
+			plyTarget.impulseData.MaceWhitelist = true
+			plyTarget:SaveData()
+
+			ply:Notify("You have given Mace whitelist to "..plyTarget:Nick()..".")
+			plyTarget:Notify("You have been given Mace Whitelist by a game moderator ("..ply:SteamName()..").")
+		else
+			return ply:Notify("Could not find player: "..tostring(name))
+		end
+    end
+}
+
+impulse.RegisterChatCommand("/givemacewhitelist", GiveMaceWhitelist)
+
+local RemoveMaceWhitelist = {
+    description = "Remove a player's Mace whitelist.",
+    requiresArg = true,
+    adminOnly = true,
+    onRun = function(ply, arg, rawText)
+        local name = arg[1]
+		local plyTarget = impulse.FindPlayer(name)
+
+		if plyTarget and plyTarget.impulseData then
+			plyTarget.impulseData.MaceWhitelist = nil
+			plyTarget:SaveData()
+
+			plyTarget:Notify("Your Mace Whitelist has been removed by a game moderator ("..ply:SteamName()..").")
+			ply:Notify("You have removed the Mace Whitelist from "..plyTarget:Nick()..".")
+		else
+			return ply:Notify("Could not find player: "..tostring(name))
+		end
+    end
+}
+
+impulse.RegisterChatCommand("/removemacewhitelist", RemoveMaceWhitelist)
+
+
+
+
+
+
+
+
+
+
+local GiveApexWhitelist = {
+    description = "Gives a player the whitelist to the Apex Rank.",
+    requiresArg = true,
+    adminOnly = true,
+    onRun = function(ply, arg, rawText)
+        local name = arg[1]
+		local plyTarget = impulse.FindPlayer(name)
+
+		if plyTarget and plyTarget.impulseData then
+
+			plyTarget.impulseData.ApexWhitelist = true
+			plyTarget:SaveData()
+
+			ply:Notify("You have given Apex whitelist to "..plyTarget:Nick()..".")
+			plyTarget:Notify("You have been given Apex Whitelist by a game moderator ("..ply:SteamName()..").")
+		else
+			return ply:Notify("Could not find player: "..tostring(name))
+		end
+    end
+}
+
+impulse.RegisterChatCommand("/giveapexwhitelist", GiveApexWhitelist)
+
+local RemoveApexWhitelist = {
+    description = "Remove a player's Apex whitelist.",
+    requiresArg = true,
+    adminOnly = true,
+    onRun = function(ply, arg, rawText)
+        local name = arg[1]
+		local plyTarget = impulse.FindPlayer(name)
+
+		if plyTarget and plyTarget.impulseData then
+			plyTarget.impulseData.ApexWhitelist = nil
+			plyTarget:SaveData()
+
+			plyTarget:Notify("Your Apex Whitelist has been removed by a game moderator ("..ply:SteamName()..").")
+			ply:Notify("You have removed the Apex Whitelist from "..plyTarget:Nick()..".")
+		else
+			return ply:Notify("Could not find player: "..tostring(name))
+		end
+    end
+}
+
+impulse.RegisterChatCommand("/removeapexwhitelist", RemoveApexWhitelist)
