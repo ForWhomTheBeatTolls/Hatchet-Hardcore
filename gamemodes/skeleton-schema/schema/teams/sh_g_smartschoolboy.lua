210b245106626154f3297039810c0ddbc93b2b06
WillMaster <79755822+willmasterLUA@users.noreply.github.com>
2024-10-03 13:16:35 +0200

    smartschoolboy faction (yes, this is real, check here: https://steamcommunity.com/sharedfiles/filedetails/?id=3342196282)

---
+++ gamemodes/skeleton-schema/schema/teams/sh_g_smartschoolboy.lua
@@ -0,0 +1,10 @@
+TEAM_SCHOOLBOY = impulse.Teams.Define({
+    name = "SmartSchoolBoy9",
+    color = Color(67, 54, 104),
+    loadout = {"impulse_hands", "weapon_physgun", "gmod_tool"},
+    description = [[One day i though of adding a new idea that hl2rp has never seen before, and as a joke i made a fake screenshot of smartschoolboy9 being a playable faction in our server. then the next day i decided to go and make an actuall smartschoolboy9 playermodel to use for hatchet. this is mainly an admin faction however, if you're reading this because you scripthooked our game, well, hi i guess? i guess you didnt expect this to be here when searching through our code, lol]],
+    salary = 0,
+    model = "models/schoolboypm/schoolboy.mdl",
+    handModel = "models/weapons/c_arms_hev.mdl",
+    percentLimit = false,
+})
