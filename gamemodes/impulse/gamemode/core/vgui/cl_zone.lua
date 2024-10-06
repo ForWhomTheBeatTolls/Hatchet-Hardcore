45e9ec6ec63e955c40a62b829ea52f07c26433cc
WillMaster <79755822+willmasterLUA@users.noreply.github.com>
2024-10-06 17:39:13 +0200

    Update cl_zone.lua

--- gamemodes/impulse/gamemode/core/vgui/cl_zone.lua
+++ gamemodes/impulse/gamemode/core/vgui/cl_zone.lua
@@ -1,14 +1,23 @@
 local PANEL = {}
 
 function PANEL:Init()
+	if IsValid(LocalPlayer().curimpulseZoneLabel) then
+		LocalPlayer().curimpulseZoneLabel:Remove()
+		LocalPlayer().curimpulseZoneLabel = nil
+	end
 	self:SetAlpha(0)
-	self:SetSize(300, 30)
-	self:AlphaTo(255, 3, 0, function() self:AlphaTo(0, 3, 4, function() self:Remove() end) end)
+	self:SetSize(ScrW(), 30)
+	self:AlphaTo(255, .4, 0, function() self:AlphaTo(0, 3, 4, function() self:Remove() end) end)
+	self.textPos = 1
+	self.nextTime = 0
+	LocalPlayer().curimpulseZoneLabel = self
 end
 
 function PANEL:Think()
 	if self.Zone != LocalPlayer():GetZoneName() then
 		self.Zone = LocalPlayer():GetZoneName()
+		self.textPos = 1
+		self.nextTime = 0
 	end
 
 	if not LocalPlayer():Alive() then
@@ -29,10 +38,26 @@ function PANEL:Think()
 	end
 end
 
+
 function PANEL:Paint(w,h)
+	-- surface.SetDrawColor(Color(100, 100, 100, 150))
+	-- surface.DrawRect(0, 0, w, h)
+
+	-- print("---------------")
+	-- print("self.zone: " .. self.Zone)
+	-- print("nextime: " .. self.nextTime)
+	-- print("textPos: " .. self.textPos)
+	-- print("string.len" .. string.len(self.Zone))
 	if self.Zone then
-		draw.DrawText(self.Zone, "Impulse-Elements23-Italic", 0, 0, color_white, TEXT_ALIGN_LEFT)
+		if CurTime() > self.nextTime and self.textPos != string.len(self.Zone) then
+			self.textPos = self.textPos + 1
+			self.nextTime = CurTime() + .08
+			surface.PlaySound("impulse/typewriter"..tostring(math.random(1,4))..".wav")
+		end
+
+		draw.DrawText(string.sub(self.Zone, 1, self.textPos), "zoneFont", ScrW() / 2 - 10, 0, color_white, TEXT_ALIGN_CENTER)
 	end
+
 end
 
 vgui.Register("impulseZoneLabel", PANEL, "DPanel")
