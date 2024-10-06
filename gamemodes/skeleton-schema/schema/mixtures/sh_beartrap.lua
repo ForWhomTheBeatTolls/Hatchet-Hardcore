9032e87ec2218cb16b8f2197bae64b13f178df37
ForWhomTheBeatTolls <155327198+ForWhomTheBeatTolls@users.noreply.github.com>
2024-10-06 14:12:45 +0300

    Add files via upload

---
+++ gamemodes/skeleton-schema/schema/mixtures/sh_beartrap.lua
@@ -0,0 +1,15 @@
+local MIX = {}
+
+MIX.Class = "beartrap"
+
+MIX.Level = 4
+MIX.Bench = "general"
+--MIX.XPMultiplier = 0.5 this will half the XP for crafting it, used to nerf the XP from simple items to stop them being spammed
+
+MIX.Output = "trap_bear"
+MIX.Input = {
+	["util_refmetal"] = {take = 3},
+	["util_recmetal"} = {take = 1},
+}
+
+impulse.RegisterMixture(MIX)
