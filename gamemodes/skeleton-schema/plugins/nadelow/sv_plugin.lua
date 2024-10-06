bc7fff5db2db80d8bf279cfa02f3151f1eacb4ac
ForWhomTheBeatTolls <155327198+ForWhomTheBeatTolls@users.noreply.github.com>
2024-10-06 18:19:16 +0300

    Update sv_plugin.lua

--- gamemodes/skeleton-schema/plugins/nadelow/sv_plugin.lua
+++ gamemodes/skeleton-schema/plugins/nadelow/sv_plugin.lua
@@ -12,11 +12,10 @@ local grenadelowcommand = {
         end
 
         if CurTime() > ply.NextGrenadeTimer then
-            local ent = ents.Create( "npc_grenade_frag" )
+            local ent = ents.Create( "m_grenade" )
             ent:SetPos(ply:EyePos() + ( ply:GetAimVector()*22 )+(ply:GetRight()*1) )
             ent:Spawn()
             ent:SetNotSolid()
-            ent:Fire("SetTimer",4,0)
             ent:GetPhysicsObject():ApplyForceCenter( ply:GetAimVector() * 300 )
             ply:ForceSequence("grendrop")
             ply.NextGrenadeTimer = CurTime() + 60
