e446c82f0817b75913d55690bcc46c693cbc739e
ForWhomTheBeatTolls <155327198+ForWhomTheBeatTolls@users.noreply.github.com>
2024-11-08 15:06:20 +0300

    Experimental medkit changes

--- gamemodes/skeleton-schema/schema/items/sh_med_kit.lua
+++ gamemodes/skeleton-schema/schema/items/sh_med_kit.lua
@@ -28,35 +28,33 @@ ITEM.UseWorkBarSound = "items/smallmedkit1.wav"
 
 local increment = 1
 function ITEM:OnUse(ply, target)
-	if (ply:Health() < ply:GetMaxHealth()) or ply:GetNWInt("BleedRate") > 0 then
+	if (ply:Health() < ply:GetMaxHealth()) or ply:GetNWInt("BleedRate") > 0 or (ply:IsHurtLimb("LLeg") or ply:IsHurtLimb("RLeg") or ply:IsHurtLimb("LArm") or ply:IsHurtLimb("RArm")) then
 	if timer.Exists(ply:EntIndex().."HealOverTime") then timer.Remove(ply:EntIndex().."HealOverTime") end
 	if timer.Exists(ply:EntIndex().."FoodPoisoning") then timer.Remove(ply:EntIndex().."FoodPoisoning") end
 	ply.FoodPoisoning = false
 	ply.HealOverTime = true
 		timer.Create(ply:EntIndex().."HealOverTime", 0.9, 40, function()
-			if ply:Alive() and ply:Health() < 100 then
-				ply:SetHealth(ply:Health() + increment)
+			if ply:Alive() then
+				if ply:Health() < 100 then
+					ply:SetHealth(ply:Health() + increment)
+				end
+				ply:GiveHealthLimb("LLeg", increment)
+				ply:GiveHealthLimb("RLeg", increment)
+				ply:GiveHealthLimb("LArm", increment)
+				ply:GiveHealthLimb("RArm", increment)
 				increment = increment + 0.1
-			else
-				timer.Remove(ply:EntIndex().."HealOverTime")
-				ply.HealOverTime = false
 			end
 		end)
 	ply:Say("/me uses a Healthkit.")
-	timer.Simple(0.1, function() if IsValid(ply) then if ply:Health() > 100 then ply:SetHealth(100) end end end)
+	timer.Simple(0.1, function() if IsValid(ply) then if ply:Health() > 100 then ply.HealOverTime = false ply:SetHealth(100) end end end)
 	else
-	ply:Notify("You can't use this, for you are not harmed.")
-	return false
+		ply:Notify("You can't use this, for you are not harmed.")
+		return false
 	end
 	
 	if ply:GetNWInt("BleedRate") != 0 then
 		ply:SetNWInt("BleedRate", ply:GetNWInt("BleedRate") - 5)
 		timer.Simple(0.1, function() if IsValid(ply) then if ply:GetNWInt("BleedRate") < 0 then ply:SetNWInt("BleedRate", 0) end end end)
-	end
-	
-	if ply:HasBrokenLegs() then
-		ply:FixLegs()
-		ply:Notify("You have healed your broken legs.")
 	end
 
 	return true
