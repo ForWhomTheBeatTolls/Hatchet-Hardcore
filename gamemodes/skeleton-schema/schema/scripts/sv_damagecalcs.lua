ca2a606984ea4704a2a2dd8840ed59a3fa47546f
ForWhomTheBeatTolls <155327198+ForWhomTheBeatTolls@users.noreply.github.com>
2024-11-12 22:42:50 +0300

    Update sv_damagecalcs.lua

--- gamemodes/skeleton-schema/schema/scripts/sv_damagecalcs.lua
+++ gamemodes/skeleton-schema/schema/scripts/sv_damagecalcs.lua
@@ -3,25 +3,15 @@
 		
 		local dmga = dmginfo:GetDamage()
 		local leg = {"LLeg", "RLeg"}
-		--ply:DoCustomAnimEvent(PLAYERANIMEVENT_FLINCH_CHEST, 1)
+		local VESTmodifier = 0.75
+		local OTAmodifier = 0.25
+		local CPmodifier = 0.7
 		
 		if dmginfo:GetAmmoType() == game.GetAmmoID("Snark") then
 			if hitgroup == HITGROUP_HEAD then
 				dmginfo:ScaleDamage(0.7)
 			end
 		end
-		
-		-- if dmginfo:GetDamageType() == DMG_FALL then
-			-- ply:TakeDamageLimb(table.Random(leg), falldamage * 1.3)
-			-- if ply:IsCrippledLimb("LLeg") and ply:GetNWBool("LLegCrippled") == false then
-				-- ply:SetNWBool("LLegCrippled", true)
-				-- ply:Notify("Your Left Leg feels numb!")
-			-- end
-			-- if ply:IsCrippledLimb("RLeg") and ply:GetNWBool("RLegCrippled") == false then
-				-- ply:SetNWBool("RLegCrippled", true)
-				-- ply:Notify("Your Left Leg feels numb!")
-			-- end
-		-- end
 
 		if hitgroup == HITGROUP_LEFTARM then
 			ply:TakeDamageLimb("LArm", dmga)
@@ -32,95 +22,105 @@
 		elseif hitgroup == HITGROUP_RIGHTLEG then
 			ply:TakeDamageLimb("RLeg", dmga)
 		end
-
-		-- print("LARMPOINTS " .. ply.Hurted_L_Arm_points)
-		-- print(ply.Hurted_L_Arm)
-		-- print("RARMPOINTS " .. ply.Hurted_R_Arm_points)
-		-- print(ply.Hurted_R_Arm)
-		-- print("RLEGPOINTS " .. ply.Hurted_R_Leg_points)
-		-- print(ply.Hurted_R_Leg)
-		-- print("LLEGPOINTS " .. ply.Hurted_L_Leg_points)
-		-- print(ply.Hurted_L_Leg)
-		 
+	
+		----------------------------
+		-- CIVIL PROTECTION BEGIN --
+		----------------------------
+		
 		if ply:Team(ply) == TEAM_CP then
-			dmginfo:ScaleDamage(0.7)
+			dmginfo:ScaleDamage(CPmodifier)
 		end
-		if ply:Team(ply) == TEAM_CP and dmginfo:GetAmmoType() == game.GetAmmoID("Snark")  then
-			dmginfo:ScaleDamage(1.5)
+		if ply:Team(ply) == TEAM_CP and dmginfo:GetAmmoType() == game.GetAmmoID("Snark")  then -- Cops VS. Melee
+			dmginfo:ScaleDamage(CPmodifier + 0.75)
 		end
-		if ply:Team(ply) == TEAM_CP and (dmginfo:GetAmmoType() == game.GetAmmoID("12mmRound"))  then
+		if ply:Team(ply) == TEAM_CP and (dmginfo:GetAmmoType() == game.GetAmmoID("12mmRound"))  then -- Cops VS. AK-47
+			dmginfo:ScaleDamage(CPmodifier + 0.75)
+		end
+		if ply:Team(ply) == TEAM_CP and (dmginfo:GetAmmoType() == game.GetAmmoID("357"))  then -- Cops VS. Revolver
+			dmginfo:ScaleDamage(CPmodifier + 0.72)
+		end
+		if ply:Team(ply) == TEAM_CP and (dmginfo:GetAmmoType() == game.GetAmmoID("AR2"))  then -- Cops VS. AR2
 			dmginfo:ScaleDamage(1.45)
 		end
-		if ply:Team(ply) == TEAM_CP and (dmginfo:GetAmmoType() == game.GetAmmoID("357"))  then
-			dmginfo:ScaleDamage(1.42)
-		end
-		if ply:Team(ply) == TEAM_CP and (dmginfo:GetAmmoType() == game.GetAmmoID("AR2"))  then
-			dmginfo:ScaleDamage(1.45)
-		end
-		if ply:Team(ply) == TEAM_CP and (dmginfo:GetAmmoType() == game.GetAmmoID("Buckshot"))  then
+		if ply:Team(ply) == TEAM_CP and (dmginfo:GetAmmoType() == game.GetAmmoID("Buckshot"))  then -- Cops VS. Shotgun
 			dmginfo:ScaleDamage(1.05)
 		end
-		if (hitgroup == HITGROUP_LEFTLEG or hitgroup == HITGROUP_RIGHTLEG or hitgroup == HITGROUP_RIGHTARM or hitgroup == HITGROUP_LEFTARM) then
-			dmginfo:ScaleDamage(2.8)
+		if ply:Team(ply) == TEAM_CP and (hitgroup == HITGROUP_HEAD) then -- Cops Head Damage
+			dmginfo:ScaleDamage(1.2)
 		end
+		
+		----------------------------
+		--- OTA REDUCTIONS BEGIN ---
+		----------------------------
+		
 		if ply:Team(ply) == TEAM_OTA then
-			dmginfo:ScaleDamage(0.25)
+			dmginfo:ScaleDamage(OTAmodifier)
 		end
-		if ply:Team(ply) == TEAM_OTA and ply:GetTeamClass() == 3 then
-			dmginfo:ScaleDamage(0.8)
+		if ply:Team(ply) == TEAM_OTA and ply:GetTeamClass() == 3 then -- OTA Mace
+			dmginfo:ScaleDamage(OTAmodifier + 0.55)
 		end
-		if ply:Team(ply) == TEAM_OTA and (dmginfo:GetAmmoType() == game.GetAmmoID("12mmRound")) then
-			dmginfo:ScaleDamage(2.1)
+		if ply:Team(ply) == TEAM_OTA and (dmginfo:GetAmmoType() == game.GetAmmoID("12mmRound")) then -- OTA Vs AK-47
+			dmginfo:ScaleDamage(OTAmodifier + 1.85)
 		end
-		if ply:Team(ply) == TEAM_OTA and (dmginfo:GetAmmoType() == game.GetAmmoID("357")) then
-			dmginfo:ScaleDamage(2.4)
+		if ply:Team(ply) == TEAM_OTA and (dmginfo:GetAmmoType() == game.GetAmmoID("357")) then -- OTA Vs Revolver
+			dmginfo:ScaleDamage(OTAmodifier + 2.15)
 		end
-		if ply:Team(ply) == TEAM_OTA and (dmginfo:GetAmmoType() == game.GetAmmoID("AR2")) then
-			dmginfo:ScaleDamage(2.6)
+		if ply:Team(ply) == TEAM_OTA and (dmginfo:GetAmmoType() == game.GetAmmoID("AR2")) then -- OTA Vs AR2
+			dmginfo:ScaleDamage(OTAmodifier + 2.35)
 		end
-		if ply:Team(ply) == TEAM_OTA and (dmginfo:GetAmmoType() == game.GetAmmoID("Buckshot")) then
-			dmginfo:ScaleDamage(1)
+		if ply:Team(ply) == TEAM_OTA and (dmginfo:GetAmmoType() == game.GetAmmoID("Buckshot")) then -- OTA Vs Shotgun
+			dmginfo:ScaleDamage(OTAmodifier + 0.75)
 		end
-		if ply:Team(ply) == TEAM_OTA and (dmginfo:GetAmmoType() == game.GetAmmoID("12mmRound")) and (hitgroup == HITGROUP_HEAD) then
-			dmginfo:ScaleDamage(0.5)
+		if ply:Team(ply) == TEAM_OTA and (dmginfo:GetAmmoType() == game.GetAmmoID("12mmRound")) and (hitgroup == HITGROUP_HEAD) then  -- OTA Vs AK-47 Headshot
+			dmginfo:ScaleDamage(OTAmodifier + 0.25)
 		end
-		if ply:Team(ply) == TEAM_OTA and (dmginfo:GetAmmoType() == game.GetAmmoID("357")) and (hitgroup == HITGROUP_HEAD) then
-			dmginfo:ScaleDamage(0.6)
+		if ply:Team(ply) == TEAM_OTA and (dmginfo:GetAmmoType() == game.GetAmmoID("357")) and (hitgroup == HITGROUP_HEAD) then  -- OTA Vs Revolver Headshot
+			dmginfo:ScaleDamage(OTAmodifier + 0.35)
 		end
-		if ply:Team(ply) == TEAM_OTA and (dmginfo:GetAmmoType() == game.GetAmmoID("Buckshot")) and (hitgroup == HITGROUP_HEAD) then
-			dmginfo:ScaleDamage(2.4)
+		if ply:Team(ply) == TEAM_OTA and (dmginfo:GetAmmoType() == game.GetAmmoID("Buckshot")) and (hitgroup == HITGROUP_HEAD) then -- OTA vs Shotgun Headshot
+			dmginfo:ScaleDamage(OTAmodifier + 2.15)
 		end
-		if ply:Team(ply) == TEAM_CP and (hitgroup == HITGROUP_HEAD) then
-			dmginfo:ScaleDamage(1.1)
+		
+		----------------------------
+		-- ARMOR REDUCTIONS BEGIN --
+		----------------------------
+		
+		if (ply.HasVest == true) and (hitgroup != HITGROUP_HEAD) then -- Vest Reduction
+			dmginfo:ScaleDamage(VESTmodifier)
 		end
-		if (ply.HasVest == true) and (hitgroup != HITGROUP_HEAD) then
-			dmginfo:ScaleDamage(0.7)
+		if (ply.HasVest == true) and (hitgroup != HITGROUP_HEAD) and dmginfo:GetAmmoType() == game.GetAmmoID("Snark")  then -- Vest VS Melee
+			dmginfo:ScaleDamage(VESTmodifier + 0.7)
 		end
-		if (ply.HasVest == true) and (hitgroup != HITGROUP_HEAD) and dmginfo:GetAmmoType() == game.GetAmmoID("Snark")  then
-			dmginfo:ScaleDamage(1.5)
+		if (ply.HasVest == true) and (hitgroup != HITGROUP_HEAD) and (dmginfo:GetAmmoType() == game.GetAmmoID("12mmRound"))  then -- Vest VS. AK-47
+			dmginfo:ScaleDamage(VESTmodifier + 0.7)
 		end
-		if (ply.HasVest == true) and (hitgroup != HITGROUP_HEAD) and (dmginfo:GetAmmoType() == game.GetAmmoID("12mmRound"))  then
-			dmginfo:ScaleDamage(1.45)
+		if (ply.HasVest == true) and (hitgroup != HITGROUP_HEAD) and (dmginfo:GetAmmoType() == game.GetAmmoID("357"))  then -- Vest VS. Revolver
+			dmginfo:ScaleDamage(VESTmodifier + 0.67)
 		end
-		if (ply.HasVest == true) and (hitgroup != HITGROUP_HEAD) and (dmginfo:GetAmmoType() == game.GetAmmoID("357"))  then
-			dmginfo:ScaleDamage(1.42)
+		if (ply.HasVest == true) and (hitgroup != HITGROUP_HEAD) and (dmginfo:GetAmmoType() == game.GetAmmoID("AR2"))  then -- Vest VS. AR2
+			dmginfo:ScaleDamage(VESTmodifier + 0.7)
 		end
-		if (ply.HasVest == true) and (hitgroup != HITGROUP_HEAD) and (dmginfo:GetAmmoType() == game.GetAmmoID("AR2"))  then
-			dmginfo:ScaleDamage(1.45)
+		if (ply.HasVest == true) and (hitgroup != HITGROUP_HEAD) and (dmginfo:GetAmmoType() == game.GetAmmoID("Buckshot"))  then -- Vest VS. Shotgun
+			dmginfo:ScaleDamage(VESTmodifier + 0.35)
 		end
-		if (ply.HasVest == true) and (hitgroup != HITGROUP_HEAD) and (dmginfo:GetAmmoType() == game.GetAmmoID("Buckshot"))  then
-			dmginfo:ScaleDamage(1.05)
+		if (ply.WeldingMask == true) and (hitgroup == HITGROUP_HEAD) then -- Vest Headshot
+			dmginfo:ScaleDamage(VESTmodifier - 0.20)
 		end
-		if (ply.WeldingMask == true) and (hitgroup == HITGROUP_HEAD) then
-			dmginfo:ScaleDamage(0.55)
+		
+		------------------------------
+		-- REGULAR REDUCTIONS BEGIN --
+		------------------------------
+		
+		if (hitgroup == HITGROUP_LEFTLEG or hitgroup == HITGROUP_RIGHTLEG or hitgroup == HITGROUP_RIGHTARM or hitgroup == HITGROUP_LEFTARM) then -- LIMB DAMAGES
+			dmginfo:ScaleDamage(2.8)
 		end
 		
 		---BLEED FUNCTIONS---
 		local rtd = math.random(1, 100)
 		if dmginfo:GetDamage() > 4 and dmginfo:GetAttacker() != ply and (dmginfo:GetAmmoType() != game.GetAmmoID("Hornet") or dmginfo:IsDamageType(DMG_SLASH) or dmginfo:IsDamageType(DMG_BLAST)) then
 			if rtd > 15 then
-			ply:SetNWInt("BleedRate",  ply:GetNWInt("BleedRate") + (dmginfo:GetDamage() / 20))
-			ply.NextBleed = CurTime() + ( 7 - (0.5 * ply:GetNWInt("BleedRate")) )
+				ply:SetNWInt("BleedRate",  ply:GetNWInt("BleedRate") + (dmginfo:GetDamage() / 20))
+				ply.NextBleed = CurTime() + ( 7 - (0.5 * ply:GetNWInt("BleedRate")) )
 			end
 		end
 		
