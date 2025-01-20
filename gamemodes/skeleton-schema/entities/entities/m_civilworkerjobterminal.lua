3c12fa937094ec5e4c8caef392d45a50e1b7809d
WillMaster <79755822+willmasterLUA@users.noreply.github.com>
2025-01-20 21:33:51 +0100

    Update m_civilworkerjobterminal.lua

--- gamemodes/skeleton-schema/entities/entities/m_civilworkerjobterminal.lua
+++ gamemodes/skeleton-schema/entities/entities/m_civilworkerjobterminal.lua
@@ -38,10 +38,11 @@ end
 	if self.NextUse < CurTime() then
 	
 		if caller:Team() == TEAM_WORKFORCE then
-	
-			net.Start("HatchetSelectWorkerJobStart")
-			net.WritePlayer(caller)
+
+			net.Start("impulse_CreateVGUI")
+			net.WriteString("hatchetCivilWorkerJob")
 			net.Send(caller)
+	
 			self:EmitSound("ambient/machines/keyboard_slow_1second.wav", 60, 100, 1, CHAN_AUTO)
 		
 		else
