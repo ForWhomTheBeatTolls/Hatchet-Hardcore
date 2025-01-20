51a313d0675a01004f4844489f675d919a50dded
ForWhomTheBeatTolls <155327198+ForWhomTheBeatTolls@users.noreply.github.com>
2025-01-20 20:03:16 +0300

    Fix for weapon reload anims not playing (I hope)

--- gamemodes/skeleton-schema/entities/weapons/m_base/shared.lua
+++ gamemodes/skeleton-schema/entities/weapons/m_base/shared.lua
@@ -70,6 +70,73 @@ function SWEP:SetupDataTables()
 	end
 end
 
+function SWEP:Reload()
+		local ply = self.Owner
+		local has, amount = ply:HasInventoryItem(self.RelAmmo)
+		local hasSpare, amountSpare = ply:HasInventoryItem(self.RelAmmo2)
+		--if self:CanReloadPrimary() then
+		--self:EmitSound(self.Primary.ReloadSound) 
+        --self.PlayAnim( ACT_VM_RELOAD )
+		
+    if self.Owner:KeyDown( IN_ATTACK ) then
+		return
+	end
+
+	if(self:Clip1() >= self:GetMaxClip1()) then
+		return
+	end
+
+		if(SERVER) then
+		if(!has and !hasSpare) then
+			ply:SetAmmo( 0, self.Primary.Ammo)
+			return
+		elseif(!has and hasSpare ) then
+			ply:SetAmmo(amountSpare, self.Primary.Ammo)
+			ply:DoReloadEvent()
+			for i = 1, amountSpare do
+				ply:TakeInventoryItemClass(self.RelAmmo2)
+				amountSpare = amountSpare - 1
+			end
+		elseif(has) then
+			ply:SetAmmo(self.Primary.ClipSize, self.Primary.Ammo)
+			ply:TakeInventoryItemClass(self.RelAmmo)
+		end
+	end
+	if(SERVER and self:Clip1() + self:Ammo1() > self.Primary.ClipSize) then
+		for i = 1, (self:Clip1() + self:Ammo1())-self.Primary.ClipSize do
+			if ply:Team() == TEAM_CP or ply:Team() == TEAM_OTA then
+				ply:GiveInventoryItem(self.RelAmmo2, 1, true)
+			else
+				ply:GiveInventoryItem(self.RelAmmo2)
+			end
+			if(amountSpare == nil) then amountSpare = 1 else amountSpare = amountSpare + 1 end
+		end
+		if(hasSpare and amountSpare >= self.Primary.ClipSize) then
+		--ply:DoReloadEvent()
+			for i = 1, self.Primary.ClipSize do
+				ply:TakeInventoryItemClass(self.RelAmmo2)
+				amountSpare = amountSpare - 1
+			end
+			if ply:Team() == TEAM_CP or ply:Team() == TEAM_OTA then
+				ply:GiveInventoryItem(self.RelAmmo, 1, true)
+				--ply:DoReloadEvent()
+			else
+				ply:GiveInventoryItem(self.RelAmmo)
+				--ply:DoReloadEvent()
+			end
+		end	
+	end
+		if(self:Clip1() < self.Primary.ClipSize and self:Ammo1() > 0) then
+			self:SetHoldType( self.HoldType )
+			self.Weapon:DefaultReload(ACT_VM_RELOAD);
+			ply:EmitSound(self.Primary.ReloadSound, 80, 100, 0.5)
+			--ply:DoReloadEvent()
+			ply:DoCustomAnimEvent( PLAYERANIMEVENT_RELOAD )
+		else
+			self:SetHoldType( self.HoldType )
+		end
+end
+
 function SWEP:Initialize()
 
 	self:SetHoldType("smg")
