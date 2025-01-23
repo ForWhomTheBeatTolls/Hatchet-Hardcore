4505ec4643c7c8c87da6c4ef4265c5984997294c
ForWhomTheBeatTolls <155327198+ForWhomTheBeatTolls@users.noreply.github.com>
2025-01-23 20:38:12 +0300

    New crosshair type

--- gamemodes/skeleton-schema/schema/scripts/cl_hudselectioncore.lua
+++ gamemodes/skeleton-schema/schema/scripts/cl_hudselectioncore.lua
@@ -3,7 +3,7 @@ AddCSLuaFile("cl_huds.lua")
 if CLIENT then
 
 impulse.DefineSetting("crosshair_radius", {name="Crosshair Radius", category="HUD", type="slider", default=8, minValue=1, maxValue=40})
-impulse.DefineSetting("crosshair_selection", {name="Crosshair Design", category="HUD", type="dropdown", default="Default", options={"Default", "Hatchet Legacy", "Half-Life 2", "Circle + Dot"}})
+impulse.DefineSetting("crosshair_selection", {name="Crosshair Design", category="HUD", type="dropdown", default="Default", options={"Default", "Hatchet Legacy", "Half-Life 2", "Circle + Dot", "Counter-Strike"}})
 impulse.DefineSetting("crosshair_color", {name="Crosshair Color", category="HUD", type="dropdown", default="White", options={"White", "Green", "Yellow", "Red", "Blue", "Purple", "Orange"}})
 
 end
