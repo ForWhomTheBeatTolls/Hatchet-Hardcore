b55885dedd5f6e4a1e5c7588fcc8883320208392
WillMaster <79755822+willmasterLUA@users.noreply.github.com>
2025-02-18 22:33:23 +0100

    Update cl_settings.lua

--- gamemodes/impulse/gamemode/core/cl_settings.lua
+++ gamemodes/impulse/gamemode/core/cl_settings.lua
@@ -1,140 +1,97 @@
---- Allows the creation, updating and reading of persistent settings
--- @module Setting
+local PANEL = {}
 
-impulse.Settings = impulse.Settings or {}
-impulse.AdvSettings = impulse.AdvSettings or {}
+function PANEL:Init()
+	local w, h = ScrW(), ScrH()
 
---- A collection of data that defines how a setting will behave
--- @realm client
--- @string name The pretty name for the setting (we'll see this in the settings menu)
--- @string category The category the setting will belong to
--- @string type tickbox, dropdown, slider, plainint
--- @param default The default value of the setting
--- @int[opt] minValue Minimum value (slider only)
--- @int[opt] maxValue Maximum value (slider only)
--- @param options A table of string options (dropdown only)
--- @func[opt] onChanged Called when setting is changed
--- @table SettingData
+	local addedCategories = {}
+	self:SetSize(w * .5, h * .7)
+	self:Center()
+	self:SetTitle("Settings")
+	self:MakePopup()
 
---- Defines a new setting for use - only call this inside the DefineSettings hook
--- @realm client
--- @string name Setting class name
--- @param settingData A table containg setting data (see below)
--- @see SettingData
-function impulse.DefineSetting(name, settingdata)
-	impulse.Settings[name] = settingdata
-	impulse.LoadSettings()
-end
+	local settingsPages = vgui.Create("DPropertySheet", self)
+	settingsPages:Dock(FILL)
 
-local toBool = tobool
-local optX = {["tickbox"] = true} -- hash comparisons faster than string
---- Gets the value of a setting
--- @realm client
--- @string name Setting class name
--- @return Setting value
-function impulse.GetSetting(name)
-	local settingData = impulse.Settings[name]
-
-	if optX[settingData.type] then
-		if settingData.value == nil then
-			return settingData.default
+	for v, k in pairs(impulse.Settings) do
+		if k.category == "ops" and not LocalPlayer():IsAdmin() then continue end -- ops settings can only be viewed by admins
+		if not addedCategories[k.category] then -- If category does not exist, create it
+			local settingSheetScroll = vgui.Create("DScrollPanel", settingsPages)
+			settingsPages:AddSheet(k.category, settingSheetScroll)
+			addedCategories[k.category] = settingSheetScroll
 		end
 
-		return toBool(settingData.value)
-	end
+		local settingBase = addedCategories[k.category]:Add("DPanel")
+		settingBase:Dock(TOP)
+		settingBase:SetPos(w * .1, h * 0.04)
+		settingBase:SetHeight(h * 0.035)
 
-	return settingData.value or settingData.default
-end
+		function settingBase:Paint(w, h)
+			surface.DrawRect(0, 0, w, 1.2)
+		end
 
---- Loads the settings from the clientside database
--- @realm client
--- @internal
-function impulse.LoadSettings()
-	for v,k in pairs(impulse.Settings) do
-		if k.type == "tickbox" or k.type == "slider" or k.type == "plainint" then
-			local def = k.default
-			if k.type == "tickbox" then 
-				def = tonumber(k.default) 
+		local settingLabel = vgui.Create("DLabel", settingBase)
+		settingLabel:SetText(k.name)
+		settingLabel:SetFont("HatchetFont-Settings")
+		settingLabel:SizeToContents()
+		settingLabel:CenterVertical()
+		settingLabel:SetPos(5, 0)
+
+		local settingType = k.type
+		if settingType == "tickbox" then
+			local tickbox = vgui.Create("DCheckBox", settingBase)
+			tickbox:CenterVertical()
+			tickbox:SetPos(w * .46, settingLabel.y + 8)
+			tickbox:SetSize(24, 24)
+			tickbox:SetValue(impulse.GetSetting(v))
+
+			function tickbox:OnChange(value)
+				impulse.SetSetting(v, value)
+
+				if k.needsRestart then
+					Derma_Message("You may need to reconnect to the server to fully activate this setting.", "impulse", "Ok")
+				end
+			end
+		elseif settingType == "plainint" then
+			local numberEntry = vgui.Create("DNumberWang", settingBase)
+			numberEntry:CenterVertical()
+			numberEntry:SetPos(w * .46, numberEntry.y + 8)
+			numberEntry:SetSize(30,20)
+			numberEntry:SetValue(impulse.GetSetting(v))
+			numberEntry:SetNumeric(true)
+
+			function numberEntry:OnValueChanged(value)
+				impulse.SetSetting(v, value)
+			end
+		elseif settingType == "slider" then
+			local numSlider = vgui.Create("DNumSlider", settingBase)
+			numSlider:CenterVertical()
+			numSlider:SetMin(k.minValue or 0)
+			numSlider:SetMax(k.maxValue or 100)
+			numSlider:SetDecimals(0)
+			numSlider:SetValue(impulse.GetSetting(v))
+			numSlider:SetSize(w * 0.17,30)
+			numSlider:SetPos(w * .32, numSlider.y)
+
+			function numSlider:OnValueChanged(value)
+				impulse.SetSetting(v, value)
+			end
+		elseif settingType == "dropdown" then
+			local dropdown = vgui.Create("DComboBox", settingBase)
+			dropdown:SetSize(120, 20)
+			dropdown:CenterVertical()
+			dropdown:SetPos(w * .3, dropdown.y)
+			dropdown:SetValue(impulse.GetSetting(v))
+			dropdown:SetSortItems(false)
+			for _, option in pairs(k.options) do
+				dropdown:AddChoice(option)
 			end
 
-			k.value = cookie.GetNumber("impulse-setting-"..v, def) -- Cache the data into a variable instead of sql so its fast
-		elseif k.type == "dropdown" or k.type == "textbox" then
-			k.value = cookie.GetString("impulse-setting-"..v, k.default)
-		end
-
-		if k.onChanged then
-			k.onChanged(k.value)
+			function dropdown:OnSelect(index, value)
+				impulse.SetSetting(v, value)
+			end
 		end
 	end
 end
 
---- Sets a setting to a specified value
--- @realm client
--- @string name Setting class name
--- @param value New value
-function impulse.SetSetting(name, newValue)
-	local settingData = impulse.Settings[name]
-	if settingData then
-		if type(newValue) == "boolean" then -- convert them boolz to intz. it's basically a gang war
-			newValue = newValue and 1 or 0
-		end
 
-		cookie.Set("impulse-setting-"..name, newValue)
-		settingData.value = newValue
-
-		if settingData.onChanged then
-			settingData.onChanged(newValue)
-		end
-
-		return
-	end
-	return print("[impulse] Error, could not SetSetting. You've probably got the name wrong! Attempted name: "..name)
-end
-
-function impulse.GetAdvSetting(name)
-	return impulse.AdvSettings[name]
-end
-
-function impulse.LoadAdvSettings()
-	if not file.Exists("impulse/adv_settings.json", "DATA") then
-		return "No adv_settings.json file found."
-	end
-	
-	local f = file.Read("impulse/adv_settings.json")
-
-	if not f then
-		return "Can't read adv_settings.json file."
-	end
-	
-	local json = util.JSONToTable(f)
-
-	if not json or not istable(json) then
-		return "Corrupted music kit file. Check formatting."
-	end
-
-	impulse.AdvSettings = json
-end
-
-impulse.LoadAdvSettings()
-
-concommand.Add("impulse_reloadadvsettings", function()
-	print("[impulse] Attempting to reload advanced settings...")
-
-	local try = impulse.LoadAdvSettings()
-
-	if try then
-		print("[impulse] Error when loading advanced settings: "..try)
-	else
-		print("[impulse] Successful reload.")
-	end
-end)
-
-concommand.Add("impulse_resetsettings", function()
-	for v,k in pairs(impulse.Settings) do
-		impulse.SetSetting(v, k.default)
-	end
-	print("[impulse] Settings reset!")
-end)
-
-
-hook.Run("DefineSettings")
+vgui.Register("impulseSettings", PANEL, "DFrame")
